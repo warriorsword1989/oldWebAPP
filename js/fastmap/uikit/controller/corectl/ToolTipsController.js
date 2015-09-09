a5dee00d9f778cfe4caa652d34eefbf67d62929b
zhongxiaoming519 <zhongxiaoming@navinfo.com>
2015-09-09 14:40:57 +0800

    修改uikit文件夹结构
    修改uikit文件夹结构

---
+++ js/fastmap/uikit/controller/corectl/ToolTipsController.js
@@ -0,0 +1,19 @@
+/**
+ * Created by zhongxiaoming on 2015/9/9.
+ */
+define(['js/fastmap/fastmap'], function (fastmap) {
+    fastmap.uiKit.ToolTipsController =  L.Class.extend({
+        options: {
+        },
+
+        initialize: function (options) {
+            this.options = options || {};
+            L.setOptions(this, options);
+        },
+
+        setCurrentTooltips:function(toolTip){
+
+        }
+
+    });
+});
