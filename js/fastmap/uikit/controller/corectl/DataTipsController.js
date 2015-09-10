4ae4369eea58b480a8a3c7f60edca32a7e6c78ba
liwanchong04719 <liwanchong@navinfo.com>
2015-09-10 10:48:27 +0800

    完善DataTip FeatCode和相关的控制类
    完善DataTip FeatCode和相关的控制类

---
+++ js/fastmap/uikit/controller/corectl/DataTipsController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by liwanchong on 2015/9/9.
+ */
+define(['js/fastmap/fastmap'],function(fastmap){
+    fastmap.DataTipsController = L.Class.extend({
+        options: {},
+        initialize: function () {
+            this.options = options || {};
+            L.setOptions(this, options);
+        },
+        setCurrentDataTip:function(dataTip) {
+
+        }
+    });
+})
