f1e34a7147f0648f8c24f90add2e5d63ffca6158
zhongxiaoming519 <zhongxiaoming@navinfo.com>
2015-09-11 14:59:45 +0800

    完成RdLink、Rdnode调整
    完成RdLink、Rdnode调整

--- js/fastmap/dataApi/ctlmodel/ContentStyle.js
+++ js/fastmap/dataApi/ctlmodel/ContentStyle.js
@@ -7,8 +7,37 @@ define(['js/fastmap/fastmap'], function (fastmap) {
             this.options = options || {};
             L.setOptions(this, options);
             this.fontSize = this.options.fontSize || 14;
-            this.fontCorlor = this.options.fontSize || 14;
-            this.position = this.options.position ;
+            this.fontCorlor = this.options.fontCorlor || 'black';
+            this.position = this.options.position || null ;
+        },
+
+        /***
+         * 设置字体颜色
+         * @param fontCorlor
+         */
+        setFontCorlor:function(fontCorlor){
+            this.fontCorlor = fontCorlor;
+        },
+
+        /***
+         * 设置字体大小
+         * @param fontSize
+         */
+        setFontSize:function(fontSize){
+            this.fontSize = fontSize;
+        },
+
+        /***
+         * 设置位置信息
+         * @param position
+         */
+        setPosition:function(position){
+            this.position = position;
         }
-    })
-})
+
+
+
+
+
+    });
+});
---
+++ js/fastmap/uikit/controller/corectl/ShapeEditorController.js
@@ -0,0 +1,101 @@
+/**
+ * Created by zhongxiaoming on 2015/9/10.
+ */
+define(['js/fastmap/fastmap'], function (fastmap) {
+    fastmap.uiKit.ShapeEditorController = L.Class.extend({
+        options: {
+        },
+
+        initialize: function (options) {
+            this.options = options || {};
+            L.setOptions(this, options);
+            this.map = options.map || null;
+            this.editType = options.editType || '';
+            this.currentEditinGeometry = null;
+        },
+
+        startEditing:function(geometry){
+            this.currentEditinGeometry = geometry;
+            this._tools(this.editType);
+        },
+
+        stopEditing:function(geometry){
+
+        },
+
+        _tools:function(type){
+            this.callback = null;
+            switch (type){
+                case 'splitLine':
+                    this.callback = function(line, splitpoint){
+                        line.split(splitpoint);
+                    };
+                    break;
+                case  'deletePoint':
+                    this.callback = function(line,point){
+                        line.deletePoint(point);
+                    }
+                    break;
+                case  'reShape':
+                    this.callback = function(line){
+                        line.reShape();
+                    }
+                    break;
+                case  'insertVetex':
+                    this.callback = function(line, point){
+                        line.insertVertex(point);
+                    }
+                    break;
+                case 'movePoint':
+                    this.callback = function(line,point){
+                        line.movePoint(point);
+                    }
+                    break;
+                case 'extendLine':
+                    this.callback = function(line, point){
+                        line.extendLine(point);
+                    }
+                    break;
+                case 'breakLine':
+                    this.callback = function(line, point){
+                        this.callback = line.breakLine(point);
+                    }
+                    break;
+                case 'copyGeometry':
+                    this.callback = function(geometry){
+                        this.geometry.copy();
+                    }
+                    break;
+                case 'mergeLine':
+                    this.callback = function(line,line){
+                        line.merge(line);
+                    }
+                    break;
+                case 'moveLine':
+                    this.callback = function(line,distance){
+                        line.move(distance);
+                    }
+                    break;
+                case 'cutline':
+                    this.callback = function(line){
+                        line.cut();
+                    }
+                    break;
+                case 'movePoint':
+                    this.callback = function(point, distance){
+                        point.move(distance);
+                    }
+                    break;
+                case 'movePolygon':
+                    this.callback = function(polygon, distance){
+                        polygon.move(distance);
+                    }
+
+
+                return this.callback;
+            }
+        }
+
+
+    });
+});
