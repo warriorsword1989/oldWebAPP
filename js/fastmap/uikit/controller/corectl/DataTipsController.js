1c1b3b5e46ea93cfb88c76f777a602fca72f8137
liwanchong04719 <liwanchong@navinfo.com>
2015-09-14 10:47:05 +0800

    添加注释
    添加注释

--- js/fastmap/uikit/controller/corectl/DataTipsController.js
+++ js/fastmap/uikit/controller/corectl/DataTipsController.js
@@ -1,19 +1,44 @@
 /**
  * Created by liwanchong on 2015/9/9.
+ * 操作dataTip
+ * @namespace fastmap
+ * @class DataTipsController
  */
 define(['js/fastmap/fastmap'], function (fastmap) {
     fastmap.DataTipsController = L.Class.extend({
+        /**
+         *相关属性
+         */
         options: {},
-        initialize: function () {
+        /**
+         *构造函数
+         * @class DataTipsController
+         * @constructor
+         * @namespace  fastmap
+         * @param options
+         */
+        initialize: function (options) {
             this.options = options || {};
             L.setOptions(this, options);
         },
+        /**
+         * 转换数据
+         * @method toDataMode
+         */
         toDataMode: function () {
 
         },
+        /**
+         * 复制数据
+         * @method copy
+         */
         copy: function () {
 
         },
+        /**
+         * 关闭窗口
+         * @method close
+         */
         close: function () {
 
         }
--- js/fastmap/uikit/controller/corectl/FeatCodeController.js
+++ js/fastmap/uikit/controller/corectl/FeatCodeController.js
@@ -1,15 +1,32 @@
 /**
  * Created by liwanchong on 2015/9/9.
+ * 元素类型
+ * @namespace fast
+ * @class featCodeController
  */
 define(['js/fastmap/fastmap'],function(fastmap) {
     fastmap.featCodeController= L.Class.extend({
+        /**
+         * 相关属性
+         */
         options:{
 
         },
+        /**
+         * 构造函数
+         * @class featCodeController
+         * @constructor
+         * @namespace  fastmap
+         */
         initialize:function() {
             this.options = options || {};
             L.setOptions(this, options);
         },
+        /**
+         * geometry代码的设置
+         * @method setFeatCode
+         * @param featCode
+         */
         setFeatCode:function(featCode) {
 
         }
