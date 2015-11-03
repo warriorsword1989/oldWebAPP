b5946e32970415fb9ec8d2466c7018b1aeb2c110
liwanchong04719 <liwanchong@navinfo.com>
2015-11-03 22:00:36 +0800

    update

--- js/ctrl/objectEditCtrl.js
+++ js/ctrl/objectEditCtrl.js
@@ -4,29 +4,85 @@
 var objectEditApp = angular.module("lazymodule", []);
 objectEditApp.controller("normalController", function ($scope) {
     var objectEditCtrl = new fastmap.uikit.ObjectEditController();
-    //$scope.$parent.$parent.rdRestrictData = objectEditCtrl.data;
+    objectEditCtrl.setOriginalData( $.extend(true,{},objectEditCtrl.data));
     $scope.rdLinkData = $scope.$parent.$parent.rdRestrictData;
     console.log("test" + $scope.rdLinkData);
     $scope.showTips = function (id) {
         alert(id);
     };
-    $scope.rdSubRestrictData = objectEditCtrl.data.details[0];
+    //初始化交限
+    $scope.addLimitedData = [
+        {"id":1},
+        {"id":2},
+        {"id":3},
+        {"id":4},
+        {"id":5}
+
+    ];
+    $scope.vehicleOptions = [
+        {"id": 0, "label": "客车(小汽车)"},
+        {"id": 1, "label": "配送卡车"},
+        {"id": 2, "label": "运输卡车"},
+        {"id": 3, "label": "步行车"},
+        {"id": 4, "label": "自行车"},
+        {"id": 5, "label": "摩托车"},
+        {"id": 6, "label": "机动脚踏两用车"},
+        {"id": 7, "label": "急救车"},
+        {"id": 8, "label": "出租车"},
+        {"id": 9, "label": "公交车"},
+        {"id": 10, "label": "工程车"},
+        {"id": 11, "label": "本地车辆"},
+        {"id": 12, "label": "自用车辆"},
+        {"id": 13, "label": "多人乘坐车辆"},
+        {"id": 14, "label": "军车"},
+        {"id": 15, "label": "有拖车的车"},
+        {"id": 16, "label": "私营公共汽车"},
+        {"id": 17, "label": "农用车"},
+        {"id": 18, "label": "载有易爆品的车辆"},
+        {"id": 19, "label": "载有水污染品的车辆"},
+        {"id": 20, "label": "载有其他污染品的车辆"},
+        {"id": 21, "label": "电车"},
+        {"id": 22, "label": "轻轨"},
+        {"id": 23, "label": "校车"},
+        {"id": 24, "label": "四轮驱动车"},
+        {"id": 25, "label": "装有防雪链的车"},
+        {"id": 26, "label": "邮政车"},
+        {"id": 27, "label": "槽罐车"},
+        {"id": 28, "label": "残疾人车"},
+        {"id": 29, "label": "预留"},
+        {"id": 30, "label": "预留"},
+        {"id": 31, "label": "标志位,禁止/允许(0/1)"}
+    ];
+    if( objectEditCtrl.data.details.length!==0) {
+        $scope.rdSubRestrictData = objectEditCtrl.data.details[0];
+    }
+
     $scope.selectTip = function (item) {
-        $scope.rdSubRestrictData = item;
+        $scope.tipsId = item;
+        var obj={};
+            obj.flag = item;
+            obj.outLinkPid =""; //$scope.rdLink.outPid;
+            obj.pid = "";//featCodeCtrl.newObj.pid;
+            obj.relationshipType = 1;
+            obj.restricInfo = 1;
+            obj.restricPid =""// featCodeCtrl.newObj.pid;
+            obj.type = 1;
+            obj.conditons = [];
+           $scope.newLimited = obj;
     };
     $scope.addTips = function () {
         if ($scope.tipsId === null || $scope.tipsId === undefined) {
             alert("请先选择tips");
             return;
         }
-        var tipsObj = $scope.rdRestrictData.info;
+        var tipsObj = $scope.rdRestrictData.details;
         for (var i = 0, len = tipsObj.length; i < len; i++) {
             if (tipsObj[i].flag === $scope.tipsId) {
                 alert("重复");
                 return;
             }
         }
-        $scope.rdRestrictData.info.push({flag: $scope.tipsId});
+        $scope.rdRestrictData.details.push( $scope.newLimited );
 
     }
     //增加时间段
@@ -38,7 +94,8 @@ objectEditApp.controller("normalController", function ($scope) {
         $scope.rdRestrictData.time.splice(id, 1);
     };
     $scope.$parent.$parent.save=function() {
-        console.log("变化后的数据" + $scope.$parent.rdLinkData);
-        objectEditCtrl.onSaved($scope.data, $scope.data);
+        objectEditCtrl.setCurrentObject($scope.rdLinkData);
+        objectEditCtrl.save();
+        console.log(objectEditCtrl.changedProperty);
     };
 });
