9bb67ee4a4978a5c3eb2297fbb85fc04a6662f99
wangmingdong <wangmingdong@navinfo.com>
2016-02-01 17:56:41 +0800

    去掉地图默认的缩放工具

--- js/app.js
+++ js/app.js
@@ -244,7 +244,10 @@ app.controller('generalController', ['$scope', '$ocLazyLoad', function ($scope, 
 }]);
 function appInit(){
 
-    map = L.map('map',{ attributionControl: false}).setView([40.012834, 116.476293], 17);
+    map = L.map('map',{ 
+        attributionControl: false,
+        zoomControl:false
+    }).setView([40.012834, 116.476293], 17);
     var layerCtrl = new fastmap.uikit.LayerController({config:Application.layersConfig});
     var highLightLayer = new fastmap.uikit.HighLightController({});
     var selectCtrl = new fastmap.uikit.SelectController();
@@ -265,7 +268,6 @@ function appInit(){
     for(var layer in layerCtrl.getVisibleLayers()){
         map.addLayer(layerCtrl.getVisibleLayers()[layer]);
     }
-
 }
 
 var map = null;
