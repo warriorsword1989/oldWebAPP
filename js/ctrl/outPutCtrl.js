f13e0fe2208ae4983a24d2d47cf8b948d9e6666d
liuzhaoxia <liuzhaoxia@navinfo.com>
2016-02-18 10:06:14 +0800

    outPut格式修改

--- js/ctrl/outPutCtrl.js
+++ js/ctrl/outPutCtrl.js
@@ -4,17 +4,25 @@
 var outPutModule = angular.module('lazymodule', []);
 outPutModule.controller('outPutController', function ($scope, $timeout) {
     $scope.outputtext = ""
+    var selectCtrl = new fastmap.uikit.SelectController();
+    var objCtrl = new fastmap.uikit.ObjectEditController();
     var output = fastmap.uikit.OutPutController();
-    $scope.outputtext=output.outPuts.join("\n----------------------------------\n");
+   // $scope.outputtext=output.outPuts.join("\n----------------------------------\n");
     output.updateOutPuts=function(){
         var outValue=output.outPuts;
         if(outValue.length===0) {
-            $scope.outputtext = "";
+            $scope.outValue = "";
         }else{
             $scope.$apply(function(){
-                $scope.outputtext = outValue.join("\n----------------------------------\n");
+               // $scope.outputtext = outValue.join("\n----------------------------------\n");
+                $scope.outValue=output.outPuts;
             })
         }
 
     }
+
+    $scope.showByMap=function(type,pid){
+        console.log(type,pid);
+    }
+
 });
