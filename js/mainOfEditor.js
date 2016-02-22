66632e94f678b74d36dcb2dceda04aaac3d39eb3
liuzhaoxia <liuzhaoxia@navinfo.com>
2016-02-22 09:19:38 +0800

    检查定位

--- js/mainOfEditor.js
+++ js/mainOfEditor.js
@@ -25,6 +25,7 @@ requirejs.config({
         'bootstrapDatepickerCN':'lib/bootstrap-datepicker/js/locales/bootstrap-datepicker.zh-CN',
         'sweet-alert':'lib/sweet-alert/js/sweet-alert.min',
         'poi-msg':'lib/poi-msg/poiMsg',
+        'mCustomScrollbar':'lib/malihu-scrollbar/jquery.mCustomScrollbar'
     },
     shim: {
         'bootstrap':['jquery'],
@@ -41,7 +42,8 @@ requirejs.config({
         'timepicki':['jquery','angular'],
         'sweet-alert':['jquery'],
         'poi-msg':['jquery'],
-        'test': ['ocLazyLoad','ngLayout','smart-table','bootstrap','select2', 'application','layers','applicationfuns','keyPressFunctions','bootspopover','bootstrapDatepicker','bootstrapDatepickerCN','timepicki','sweet-alert','poi-msg']
+        'mCustomScrollbar':['jquery'],
+        'test': ['ocLazyLoad','ngLayout','smart-table','bootstrap','select2', 'application','layers','applicationfuns','keyPressFunctions','bootspopover','bootstrapDatepicker','bootstrapDatepickerCN','timepicki','sweet-alert','poi-msg','mCustomScrollbar']
     }
 });
 
