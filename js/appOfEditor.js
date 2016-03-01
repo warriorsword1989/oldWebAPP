1c3dfb9a3eb292832139a5b4904e7c50a5942a1d
wangtun <wangtun@navinfo.com>
2016-03-01 16:26:42 +0800

    更改

--- js/appOfEditor.js
+++ js/appOfEditor.js
@@ -288,12 +288,12 @@ function appInit(){
         zoomControl:false
     }).setView([40.012834, 116.476293], 17);
     /*增加比例尺*/
-    var scale = L.control.scale({
-        metric:true,
-        imperial:false,
-        position:'bottomleft',
-        updateWhenIdle:true
-    }).addTo(map);
+//    var scale = L.control.scale({
+//        metric:true,
+//        imperial:false,
+//        position:'bottomleft',
+//        updateWhenIdle:true
+//    }).addTo(map);
     var layerCtrl = new fastmap.uikit.LayerController({config:Application.layersConfig});
     var highLightLayer = new fastmap.uikit.HighLightController({});
     var selectCtrl = new fastmap.uikit.SelectController();
