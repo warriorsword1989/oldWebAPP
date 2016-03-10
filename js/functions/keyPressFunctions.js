42bc0f3f0b3b95b2232dcede26bf1e45b5406a2f
zhongxiaoming519 <zhongxiaoming@navinfo.com>
2016-03-10 15:58:43 +0800

    解决不能删除形状点问题

--- js/functions/keyPressFunctions.js
+++ js/functions/keyPressFunctions.js
@@ -343,8 +343,8 @@ function keyEvent(ocLazyLoad, scope) {
                         }
                         var snapObj = selectCtrl.getSnapObj();
                         //var nodePid = null;
-                        var interLinks =snapObj.interLinks.length!=0?snapObj.interLinks: [];
-                        var interNodes = snapObj.interNodes.length!=0?snapObj.interNodes: [];
+                        var interLinks =(snapObj&&snapObj.interLinks.length!=0)?snapObj.interLinks: [];
+                        var interNodes = (snapObj&&snapObj.interNodes.length!=0)?snapObj.interNodes: [];
                         var param = {
                             "command": "REPAIR",
                             "type": "RDLINK",
