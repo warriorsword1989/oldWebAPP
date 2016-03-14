e6f14684b981afc02b205b0edbfdd1629e53aa62
wangtun <wangtun@navinfo.com>
2016-03-14 19:45:54 +0800

    增加车信对象

---
+++ js/fastmap/dataApi/datamodel/Rdlaneconnexity.js
@@ -0,0 +1,79 @@
+/**
+ * Created by wangtun on 2016/3/14.
+ */
+fastmap.dataApi.rdLaneConnexity=fastmap.dataApi.GeoDataModel.extend({
+    pid:null,
+    inLinkPid:null,
+    nodePid:null,
+    laneInfo:"",
+    conflictFlag:0,
+    kgFlag:0,
+    laneNum:0,
+    leftExtend:0,
+    rightExtend:0,
+    srcFlag:0,
+    topos:[],
+
+    initialize: function (data, options) {
+        L.setOptions(this, options);
+        this.setAttributeData(data);
+    },
+
+    setAttributeData:function(data){
+        this.pid = data["pid"];
+        this.inLinkPid = data["inLinkPid"];
+        this.nodePid = data["nodePid"];
+        this.laneInfo = data["laneInfo"];
+        this.conflictFlag = data["conflictFlag"];
+        this.kgFlag = data["kgFlag"];
+        this.laneNum = data["laneNum"];
+        this.leftExtend = data["leftExtend"];
+        this.rightExtend = data["rightExtend"];
+        this.srcFlag = data["srcFlag"];
+
+        for(var i=0;i<data["topos"].length;i++){
+            var topos= new fastmap.dataApi.rdLaneTopology(data["topos"][i]);
+            this.topos.push(topos);
+        }
+    },
+
+    getSnapShot:function(){
+        var data={};
+        data["pid"] = this.pid;
+        data["inLinkPid"] = this.inLinkPid;
+        data["nodePid"] = this.nodePid;
+        data["laneInfo"] = this.laneInfo;
+        data["conflictFlag"] = this.conflictFlag;
+        data["kgFlag"] = this.kgFlag;
+        data["laneNum"] = this.laneNum;
+        data["leftExtend"] = this.leftExtend;
+        data["rightExtend"] = this.rightExtend;
+        data["srcFlag"] = this.srcFlag;
+        data["topos"]=[];
+        for(var i=0;i<this.topos.length;i++){
+            data["topos"].push(this.topos[i].getIntegrate())
+        }
+
+        return data;
+    },
+
+    getIntegrate:function(){
+        var data={};
+        data["pid"] = this.pid;
+        data["inLinkPid"] = this.inLinkPid;
+        data["nodePid"] = this.nodePid;
+        data["laneInfo"] = this.laneInfo;
+        data["conflictFlag"] = this.conflictFlag;
+        data["kgFlag"] = this.kgFlag;
+        data["laneNum"] = this.laneNum;
+        data["leftExtend"] = this.leftExtend;
+        data["rightExtend"] = this.rightExtend;
+        data["srcFlag"] = this.srcFlag;
+        data["topos"]=[];
+        for(var i=0;i<this.topos.length;i++){
+            data["topos"].push(this.topos[i].getIntegrate())
+        }
+
+        return data;
+    }
+})
