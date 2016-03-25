ce7de25653df1077ab7f46132dab94eb8d95598d
wangmingdong <wangmingdong@navinfo.com>
2016-03-25 14:31:56 +0800

    交限属性，根据限制类型判断时间段控件的显示

--- js/ctrl/restrictionCtrl/rdRestriction.js
+++ js/ctrl/restrictionCtrl/rdRestriction.js
@@ -18,6 +18,34 @@ objectEditApp.controller("normalController", function ($scope, $timeout, $ocLazy
     if (highLightLayer.highLightLayersArr.length !== 0) {
         highLightLayer.removeHighLightLayers();
     }
+    /*时间控件*/
+    $scope.fmdateTimer = function (str) {
+        $scope.$on('get-date', function (event, data) {
+            $scope.codeOutput = data;
+            $scope.rdRestrictData.time = data;
+        });
+        $timeout(function () {
+            $scope.$broadcast('set-code', str);
+            $scope.codeOutput = str;
+            $scope.rdRestrictData.time = str;
+            $scope.$apply();
+        }, 100);
+    }
+    /*改变限制类型判断时间控件*/
+    $scope.changeLimitType = function(type){
+        if(type == 2){
+            $timeout(function(){
+                $ocLazyLoad.load('ctrl/fmdateTimer').then(function () {
+                    $scope.dateURL = 'js/tepl/fmdateTimer.html';
+                    /*查询数据库取出时间字符串*/
+                    var tmpStr = $scope.rdRestrictData.time;
+                    $scope.fmdateTimer(tmpStr);
+                });
+            });
+        }else{
+            $scope.dateURL = '';
+        }
+    }
     //初始化数据
     $scope.initializeData = function () {
         objectEditCtrl.setOriginalData(objectEditCtrl.data.getIntegrate());
@@ -55,6 +83,10 @@ objectEditApp.controller("normalController", function ($scope, $timeout, $ocLazy
         })
         //初始化交限中的第一个禁止方向的信息
         $scope.rdSubRestrictData = objectEditCtrl.data.details[0];
+        /*如果默认限制类型为时间段禁止，显示时间段控件*/
+        if($scope.rdSubRestrictData.type == 2){
+            $scope.changeLimitType(2);
+        }
     };
 
 
@@ -250,26 +282,6 @@ objectEditApp.controller("normalController", function ($scope, $timeout, $ocLazy
     $scope.minusTime = function (id) {
         $scope.rdRestrictData.time.splice(id, 1);
     };
-    $timeout(function () {
-        $ocLazyLoad.load('ctrl/fmdateTimer').then(function () {
-            $scope.dateURL = 'js/tepl/fmdateTimer.html';
-            /*查询数据库取出时间字符串*/
-            var tmpStr = (!$scope.rdRestrictData.time) ? '' : $scope.rdRestrictData.time;
-            // var tmpStr = '[[(h7m40)(h8m0)]+[(h11m30)(h12m0)]+[(h13m40)(h14m0)]+[(h17m40)(h18m0)]+[(h9m45)(h10m5)]+[(h11m45)(h12m5)]+[(h14m45)(h15m5)]+[[(M6d1)(M8d31)]*[(h0m0)(h5m0)]]+[[(M1d1)(M2d28)]*[(h0m0)(h6m0)]]+[[(M12d1)(M12d31)]*[(h0m0)(h6m0)]]+[[(M1d1)(M2d28)]*[(h23m0)(h23m59)]]+[[(M12d1)(M12d31)]*[(h23m0)(h23m59)]]]';
-            $scope.fmdateTimer(tmpStr);
-        });
-    })
-    /*时间控件*/
-    $scope.fmdateTimer = function (str) {
-        $scope.$on('get-date', function (event, data) {
-            $scope.codeOutput = data;
-        });
-        $timeout(function () {
-            $scope.$broadcast('set-code', str);
-            $scope.codeOutput = str;
-            $scope.$apply();
-        }, 100);
-    }
     //修改属性
     $scope.save = function () {
         var index = $(".show-tips.active").attr('data-index');
