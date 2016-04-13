8b364c25bfeb977dddc417c1a214c111b2ecdf1a
liwanchong04719 <liwanchong@navinfo.com>
2016-04-13 14:45:57 +0800

    解决冲突

--- js/ctrl/layers_switch_ctrl/sceneLayersCtrl.js
+++ js/ctrl/layers_switch_ctrl/sceneLayersCtrl.js
@@ -5,7 +5,13 @@ var sceneLayersModule = angular.module('lazymodule', []);
 sceneLayersModule.controller('sceneLayersController', function ($scope) {
     var layerCtrl = new fastmap.uikit.LayerController();
     var eventController = fastmap.uikit.EventController();
-    $scope.items = layerCtrl.layers;
+    $scope.scenceArr = [
+        {"id": 1, "label": "普通限速", "selected": false},
+        {"id": 2, "label": "条件限速", "selected": false},
+        {"id": 3, "label": "互联网Rtic", "selected": false},
+        {"id": 4, "label": "行政区划", "selected": false},
+        {"id": 5, "label": "复杂要素", "selected": false}
+    ]
     var outLayers = [];
     for (var i = 0; i < layerCtrl.layers.length; i++) {
         if (layerCtrl.layers[i].options.groupid == "dataLayers") {
@@ -13,25 +19,66 @@ sceneLayersModule.controller('sceneLayersController', function ($scope) {
         }
     }
     $scope.items = outLayers;
-
-    $scope.showLayers = function (item) {
-        if (map.hasLayer(layerCtrl.getLayerById("mesh"))) {
-
-        } else {
-            map.addLayer(layerCtrl.getLayerById("mesh"));
-        }
-
-        for (var layer in layerCtrl.layers) {
-            if (item.options['singleselect'] == true) {
-                if (layerCtrl.layers[layer].options['singleselect'] === true && item.options.visible != true) {
-                    layerCtrl.layers[layer].options.visible = false
-                }
+    $scope.resetLayers=function() {
+        for (var i = 0; i < layerCtrl.layers.length; i++) {
+            if (layerCtrl.layers[i].options.groupid == "dataLayers") {
+              if(layerCtrl.layers[i].options.type==="rdrticPoint") {
+                  layerCtrl.layers[i].options.visible = false;
+              }else{
+                  layerCtrl.layers[i].options.visible = true;
+              }
             }
-
         }
+    };
+    $scope.originLayers = $.extend({},layerCtrl.layers);
+    $scope.showLayers = function (item) {
         //单击checkbox的处理
         item.options.visible = !item.options.visible;
         eventController.fire(eventController.eventTypes.LAYERONSWITCH, {layerArr: layerCtrl.layers});
 
     };
+    $scope.ordSpeedScene=function() {
+
+    };
+    $scope.conditionSpeedScene=function() {
+    };
+    $scope.rticSecne=function() {
+        for (var i= 0,len=$scope.items.length;i<len;i++) {
+            if($scope.items[i].options.id==="rdrtic"||$scope.items[i].options.id==="referenceLine") {
+                $scope.items[i].options.visible = true;
+            }else{
+                $scope.items[i].options.visible = false;
+            }
+        }
+    };
+    $scope.showScene = function (item, event) {
+
+        for (var scene in $scope.scenceArr) {
+            if (item['selected'] != true) {
+                $scope.scenceArr[scene]['selected'] = false
+            }
+        }
+        item['selected']  = !item['selected'] ;
+        if(item['selected']) {
+            $scope.resetLayers();
+            switch (item["id"]) {
+                case 1://普通限速
+                    $scope.ordSpeedScene();
+                    break;
+                case 2://条件限速
+                    $scope.conditionSpeedScene();
+                    break;
+                case 3://互联网rtic
+                    $scope.rticSecne();
+                    break;
+                case 4:
+                    break;
+            }
+        }else{
+            $scope.resetLayers();
+        }
+        eventController.fire(eventController.eventTypes.LAYERONSWITCH, {layerArr: layerCtrl.layers});
+
+    };
+
 })
