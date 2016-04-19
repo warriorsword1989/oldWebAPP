1c031d411b046defcfe86793c909027db7c9a276
wangmingdong <wangmingdong@navinfo.com>
2016-04-19 21:16:03 +0800

    立交属性面板编辑

--- scripts/components/road/ctrls/attr_rdgsc_ctrl/rdGscCtrl.js
+++ scripts/components/road/ctrls/attr_rdgsc_ctrl/rdGscCtrl.js
@@ -7,14 +7,14 @@ rdGscApp.controller("rdGscController",function($scope) {
     var objCtrl = fastmap.uikit.ObjectEditController();
     var eventController = fastmap.uikit.EventController();
 
-    var test = {"pid":66666, "geometry":"56.66,77.98","processFlag":1,"links":[
+   /* var test = {"pid":66666, "geometry":"56.66,77.98","processFlag":1,"links":[
             {
                 "pid":66666,"zlevel":1,"linkPid":1121,"startEnd":1,"tableName":"RD_LINK"
             },
             {
                 "pid":66666,"zlevel":2,"linkPid":2332,"startEnd":2,"tableName":"LC_LINK "
             }
-        ]};
+        ]};*/
 
     /*处理标识*/
     $scope.processFlag = [
@@ -36,8 +36,8 @@ rdGscApp.controller("rdGscController",function($scope) {
         {"id": "CMG_BUILDLINK", "label": "CMG_BUILDLINK"}
     ];
     $scope.initializeData = function(){
-        var gscData = fastmap.dataApi.rdgsc(test);
-        objCtrl.data = gscData;
+        /*var gscData = fastmap.dataApi.rdgsc(test);
+        objCtrl.data = gscData;*/
         $scope.reGscData = objCtrl.data;
         objCtrl.setOriginalData(objCtrl.data.getIntegrate());
     };
