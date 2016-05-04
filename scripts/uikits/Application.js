b2f451f513b56d21af60e0bf378a83cfdefce007
wangmingdong <wangmingdong@navinfo.com>
2016-05-04 18:38:10 +0800

    token

--- scripts/uikits/Application.js
+++ scripts/uikits/Application.js
@@ -4,7 +4,7 @@ var App = {};
 // web app全局配置信息
 App.Config = {
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "0000029900O6MPLEE3D056997172E0796F234F4F805268B7",
+    accessToken: "0000029900O6OKMW2B3458B7BDF0350CDD4C365086268415",
 };
 // web app的公用函数命名空间
 App.Util = {};
