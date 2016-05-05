c91edd8933c381efd0b7731ca6ad867cf86dd0a4
liwanchong04719 <liwanchong@navinfo.com>
2016-05-05 17:05:24 +0800

    修改问题

--- scripts/components/road/ctrls/toolBar_cru_ctrl/selectAdShapeCtrl.js
+++ scripts/components/road/ctrls/toolBar_cru_ctrl/selectAdShapeCtrl.js
@@ -1,5 +1,5 @@
 /**
- * Created by zhaohang on 2016/4/12.
+ * Created by zhaohang on 2016/04/12.
  */
 var selectAdApp = angular.module("mapApp", ['oc.lazyLoad']);
 selectAdApp.controller("selectAdShapeController", ["$scope", '$ocLazyLoad', '$rootScope', function ($scope, $ocLazyLoad, $rootScope) {
