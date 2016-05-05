77add1035e47a2dfbabcbac1d3af796f74222506
maliNavinfo@navinfo.com <maliNavinfo@navinfo.com>
2016-05-05 18:05:34 +0800

    项目列表界面修改提交
    项目列表界面修改提交

---
+++ scripts/components/poi/ctrls/data-list/headCtl.js
@@ -0,0 +1,30 @@
+angular.module('app',['oc.lazyLoad', 'ui.bootstrap', 'dataService']).controller('headCtl', ['$scope', '$ocLazyLoad', '$rootScope', '$q', 'poi', function($scope, $ocll, $rs, $q, poi) {
+    $scope.goback = function() {
+        var pathName = window.document.location.pathname;
+        var appName = pathName.substring(0, pathName.indexOf('/poibase'));
+        alert('pathName:' +pathName)
+        alert('appName:'+appName)
+        if (pathName == appName + "/poibase/projecManage.html") {
+            this.logout();
+        } else {
+            window.document.location.href = window.document.referrer;
+        }
+    };
+    $scope.logout = function() {
+    	var appPath = window.document.location.pathname;
+        var appName = appPath.substring(0, appPath.indexOf('/poibase'));
+        window.location.href = appName + "/poibase/login.html";
+        this.clearCookie();
+    };
+    $scope.realName = (function () {
+    	var realName = "";
+    	var arr = document.cookie.split('; ');
+    	for(var i=0;i<arr.length;i++){
+    		if(arr[i].indexOf('FM_USER_NAME') > -1){
+    			realName = arr[i].split('=')[1];
+    			break;
+    		}
+    	}
+    	return realName;
+    }());
+}]);
--- scripts/uikits/poibase/projectManageCtl.js
+++ scripts/uikits/poibase/projectManageCtl.js
@@ -1,26 +1,7 @@
-angular.module('app', ['oc.lazyLoad', 'ui.bootstrap', 'dataService']).controller('projectManageCtl', ['$scope', '$ocLazyLoad', '$rootScope', '$q', 'poi','meta', function($scope, $ocll, $rs, $q, poi,meta) {
-    $scope.meta = {};
-    var promises = [];
-    promises.push(meta.getKindList().then(function(data) {
-        $scope.meta.kindList = data;
-    }));
-    promises.push(poi.getPoiDetailByFid("00005920160427213444").then(function(data) {
-        $scope.poi = data;
-    }));
-    promises.push(poi.getPoiList().then(function(data) {
-        $scope.poiList = data;
-    }));
-    $scope.test = function() {
-        console.log("main");
-        poi.test();
-    };
-    $q.all(promises).then(function() {
-        $ocll.load('../../scripts/components/poi/ctrls/attr-base/generalBaseCtl.js').then(function() {
-            $scope.baseInfoTpl = '../../scripts/components/poi/tpls/attr-base/generalBaseTpl.html';
-            $scope.$on('$includeContentLoaded', function($event) {
-                $scope.$broadcast("loadup", $scope.poi);
-            });
-        });
+angular.module('app', ['oc.lazyLoad', 'ui.bootstrap', 'dataService']).controller('projectManageCtl', ['$scope', '$ocLazyLoad', '$rootScope', '$q', function($scope, $ocll, $rs, $q) {
+	$ocll.load("components/poi/ctrls/data-list/headCtl").then(function() {
+        $scope.headTpl = "../../scripts/components/poi/tpls/data-list/header.html";
     });
+	
 
 }]);
