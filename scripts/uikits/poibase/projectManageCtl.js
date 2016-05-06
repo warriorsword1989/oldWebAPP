2861c12d20a3c9c28e77913740beb9bba009a698
凌龙 <linglong@navinfo.com>
2016-05-06 20:26:49 +0800

    改错

--- scripts/uikits/poibase/projectManageCtl.js
+++ scripts/uikits/poibase/projectManageCtl.js
@@ -1,4 +1,4 @@
-angular.module('app', ['oc.lazyLoad', 'ui.bootstrap', 'ngTable']).controller('projectManageCtl', ['$scope', '$ocLazyLoad', '$rootScope', '$q','NgTableParams', function($scope, $ocll, $rs, $q) {
+angular.module('app', ['oc.lazyLoad', 'ui.bootstrap', 'ngTable']).controller('projectManageCtl', ['$scope', '$ocLazyLoad', '$rootScope', '$q', function($scope, $ocll, $rs, $q) {
 	$ocll.load("components/poi/ctrls/data-list/headCtl").then(function() {
         $scope.headTpl = "../../scripts/components/poi/tpls/data-list/header.html";
     });
