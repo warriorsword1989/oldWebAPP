4c79812d5758afa18bd3bc5b96010937af20a35a
wuzhen <wuzhen@navinfo.com>
2016-05-06 15:17:27 +0800

    修改token

--- scripts/uikits/Application.js
+++ scripts/uikits/Application.js
@@ -4,7 +4,7 @@ var App = {};
 // web app全局配置信息
 App.Config = {
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "0000029900O6SG34A910FDBA20F070326D6EA09043081D17",
+    accessToken: "0000029900O6SLD80B329858047344C54FEFBD46C17FD80A",
 };
 // web app的公用函数命名空间
 App.Util = {};
