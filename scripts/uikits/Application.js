787626dd703e94fec8b5cbb21385e31af40553fc
wangmingdong <wangmingdong@navinfo.com>
2016-05-07 11:25:07 +0800

    更新token，controller名称小写

--- scripts/uikits/Application.js
+++ scripts/uikits/Application.js
@@ -4,7 +4,7 @@ var App = {};
 // web app全局配置信息
 App.Config = {
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "000002TJ00O6SD5C37DC4DD27BFDEE099A6546E0F1E5C193",
+    accessToken: "0000029900O6U900E58CCD940165AD491F30AABD220F8D69",
     hbaseServiceUrl: "http://fastmap.navinfo.com/fos/datum"
 };
 // web app的公用函数命名空间
