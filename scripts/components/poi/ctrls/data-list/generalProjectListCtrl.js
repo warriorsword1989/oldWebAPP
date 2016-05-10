6a836cdcf684f8f5a3df4a4ada4986e2034e2524
凌龙 <linglong@navinfo.com>
2016-05-10 17:52:37 +0800

    增加表格功能

--- scripts/components/poi/ctrls/data-list/generalProjectListCtrl.js
+++ scripts/components/poi/ctrls/data-list/generalProjectListCtrl.js
@@ -1,35 +1,50 @@
 angular.module('app').controller('commonCtrl', ['$scope', 'uibButtonConfig', 'NgTableParams','$timeout','$sce',  function (scope, uibBtnCfg, NgTableParams,$timeout,$sce) {
     var _self = scope;
-    console.log(scope)
     uibBtnCfg.activeClass = "btn-success";
     scope.radioModel = 'myProject';
     scope.radio_select = '全局搜索';
 
     scope.$on("currentProjectList", function(event, data) {
         _self.currentData = data;
-        console.log('获取数据')
+        console.log('获取我的项目数据')
+        _self.filterData(_self.currentData)
+        _self.constructTable();
+    })
+    scope.$on("hisProjectList", function(event, data) {
+        _self.historytData = data;
+        console.log('获取历史项目数据')
+        _self.filterData(_self.historytData)
+        _self.constructTable();
+    })
+    //给返回数据增加序号索引;
+    scope.filterData = function(data){
         for(var i=0;i<data.length;i++){
             data[i].num_index = i+1;
         }
-        _self.tableParams = new NgTableParams({count:10,filter:{'projectName':''}}, {counts: [],paginationMaxBlocks:13,paginationMinBlocks: 2,dataset: _self.currentData});
-
+    }
+    scope.constructTable = function(){
         scope.$watch('radioModel',function(newValue,oldValue,scope){
-            if(newValue=='myProject'){
                 _self.cols = [
                     { field: "num_index", title: "序号", sortable: "num_index", show: true},
                     { field: "projectId", title: "项目编号", sortable: "projectId", show: false},
                     { field: "projectName", title: "项目名称", sortable: "projectName", show: true,getValue:handleProjectName},
-                    { field: "projectType", title: "项目类型	", sortable: "projectType", show: true,getValue:handleProjectType},
+                    { field: "projectType", title: "类型	", sortable: "projectType", show: true,getValue:handleProjectType},
                     { field: "projectScheduleCreate", title: "创建时间", sortable: "projectScheduleCreate", show: true},
                     { field: "projectScheduleBegin", title: "开始时间", sortable: "projectScheduleBegin", show: true},
                     { field: "projectScheduleFinish", title: "结束时间", sortable: "projectScheduleFinish", show: true},
                     { field: "projectPhase", title: "状态", sortable: "projectPhase", show: true,getValue:handleProjectPhase},
                     { field: "editSupportId", title: "编辑池", sortable: "editSupportId", show: false}
                 ];
+                console.log(_self.cols)
                 //格式化项目名称
                 function handleProjectName($scope,row){
                     var value = row[this.field];
-                    var html = "<a href='https://www.google.co.uk/search?q=" + value + "' target='_blank'>" + value + "</a>";
+                    value = value.length>20?value.substr(0,20)+'...':value;
+                    if(value.length>20){
+                        var html = '<a uib-tooltip="Hello, World!" title="row[this.field]" tooltip-trigger="focus" href="./projectInfo.html?access_token='+App.Config.accessToken+'&projectId='+row.projectId+'" target="_blank">'+value+'</a>'
+                    }else{
+                        var html = '<a uib-tooltip="Hello, World!" tooltip-trigger="focus" href="./projectInfo.html?access_token='+App.Config.accessToken+'&projectId='+row.projectId+'" target="_blank">'+value+'</a>'
+                    }
                     return $sce.trustAsHtml(html);
                 }
                 //格式化项目类型
@@ -49,56 +64,40 @@ angular.module('app').controller('commonCtrl', ['$scope', 'uibButtonConfig', 'Ng
                         default: return '未定义';
                     }
                 }
-
-                console.log(scope.radio_select)
                 //初始化ng-table;
+                if(newValue=='myProject'){
+                    console.log('myProject')
+                    _self.tableParams = new NgTableParams({page:1,count:15,filter:{'projectName':''}}, {counts: [],paginationMaxBlocks:13,paginationMinBlocks: 2,dataset: _self.currentData});
+                }else{
+                    console.log('historyProject')
+                    _self.tableParams = new NgTableParams({count:10,filter:{'projectName':''}}, {counts: [],paginationMaxBlocks:13,paginationMinBlocks: 2,dataset: _self.historytData});
+                }
 
-                //_self.tableParams = new NgTableParams({count:10,filter:{'projectName':''}}, {counts: [],paginationMaxBlocks:13,paginationMinBlocks: 2,dataset: _self.currentData});
                 var timeout = null;
                 scope.$watch('search_text',function(newValue,oldValue,scope){
-                    console.log('变化')
                     var search_type = '';
                     if(timeout)$timeout.cancel(timeout);
                     if(newValue!=oldValue){
+                        console.log('变化')
                         timeout = $timeout(function() {
                             if(scope.radio_select=='全局搜索'){
                                 _self.tableParams.filter({$: _self.search_text});
                             }else{
                                 var filter = {};
                                 switch (scope.radio_select){
-                                    case '项目名称':
-                                        search_type = 'projectName';
-                                        break;
-                                    case '项目类型':
-                                        search_type = 'projectType';
-                                        break;
-                                    case '项目状态':
-                                        search_type = 'projectPhase';
-                                        break;
-
+                                    case '项目名称':search_type = 'projectName';break;
+                                    case '项目类型':search_type = 'projectType';break;
+                                    case '项目状态':search_type = 'projectPhase';break;
                                 }
                                 filter[search_type] = newValue;
                                 angular.extend(_self.tableParams.filter(), filter);
                             }
-                        }, 500);
+                        }, 300);
                     }
 
                 })
-
-            }else if(newValue=='historyProject'){
-                console.log("初始化表2")
-                var data = [{ name: '凌龍', age: 21 }, { name: '卡卡', age: 88 }];
-                _self.cols = [
-                    { field: "name", title: "姓名", filter: { name: "text" }, show: true },
-                    { field: "age", title: "年龄", filter: { age: "number" }, show: true }
-                ];
-                //_self.tableParams = new NgTableParams({count: 1}, {dataset: data});
-            }else{
-                return false
-            }
         })
-    })
-
+    }
 
 
 }]);
