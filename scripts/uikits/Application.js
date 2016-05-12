ada8824b96bbbf4e1d5353f478c9c396ef65bbdd
凌龙 <linglong@navinfo.com>
2016-05-12 10:44:56 +0800

    修改下拉列表收起事件

--- scripts/uikits/Application.js
+++ scripts/uikits/Application.js
@@ -4,7 +4,7 @@ var App = {};
 // web app全局配置信息
 App.Config = {
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "0000029900O71JD0458D598AD6626549038BEEC8E2C6FF0A",
+    accessToken: "0000029900O73CHKD2953C82067C3A6E58ED4B71FC1307A7",
     hbaseServiceUrl: "http://fastmap.navinfo.com/fos/datum"
 };
 // web app的公用函数命名空间
