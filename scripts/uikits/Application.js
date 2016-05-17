8f308ac20f2bc32d4c166f7531032c91b3a06396
wangmingdong <wangmingdong@navinfo.com>
2016-05-17 13:49:25 +0800

    增加appType

--- scripts/uikits/Application.js
+++ scripts/uikits/Application.js
@@ -6,7 +6,8 @@ App.Config = {
     serviceUrl: "http://192.168.4.189/fos",
     accessToken: "0000029900O7CMD9032F0AF65935A3C83CB72E452927E391",
     hbaseServiceUrl: "http://fastmap.navinfo.com/fos/datum",
-    resourceUrl: "http://192.168.4.189/resources"
+    resourceUrl: "http://192.168.4.189/resources",
+    appType: "WEB"
 };
 // web app的公用函数命名空间
 App.Util = {};
