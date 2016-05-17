8532b2ef6c4b7788615d12b53ee604f332efdc0c
wangtun <wangtun@navinfo.com>
2016-05-17 14:29:41 +0800

    演示页面搭建

--- apps/roadnet/selectPro.js
+++ apps/roadnet/selectPro.js
@@ -1,8 +1,11 @@
+var accessToken="";
 $(document).ready(function(){
+	accessToken=window.location.href.split("=")[1];
 	var param = {
-		"userId":1
+		"userId":1,
+		"projectId":11
 	};
-	$.get(Application.url+'/man/project/getByUser?parameter='+JSON.stringify(param),function(data){
+	$.get(Application.url+'/project/grid/getByUser?parameter='+JSON.stringify(param),function(data){
         data = JSON.parse(data);
         if(data.errcode == 0){
 			initProHtml(data.data);
@@ -13,42 +16,67 @@ $(document).ready(function(){
 });
 function initProHtml(projects){
 	var _html = [];
-	$.each(projects,function(i,v){
-		_html.push('<div class="box col-md-3 col-lg-3">');
-		_html.push('<div class="img_box">');
-        if(v.projectName.indexOf("tianjin")>-1){
-            _html.push('<img class="img_box_img" src="css/img/tianjin.png" alt="');
-        }
-        else{
-            _html.push('<img class="img_box_img" src="css/img/beijing.png" alt="');
-        }
 
-		_html.push(v.projectName);
-		_html.push('">');
-		_html.push('<div class="img_box_caption">');
-		_html.push('<h3 class="img_box_caption_h">');
-		_html.push(v.projectName);
-		_html.push('</h3>');
-		_html.push('<p class="img_box_caption_p">');
-		if(v.tipsNum){
-			_html.push('待作业Tips总量 <span class="label label-info">');
-			_html.push('1234');
-			_html.push('</span> 个</br>');
-		}
-		_html.push('项目ID：<span class="label label-info">');
-		_html.push(v.projectId);
-		_html.push('</span>');
-		_html.push('</p>');
-		_html.push('<a class="img_box_caption_a" href="project.html?proId=');
-		_html.push(v.projectId);
-		_html.push('"></a>');
-		_html.push('</div>');
-		_html.push('</div>');
-		_html.push('<div class="img_box_title">');
-		_html.push(v.projectName);
-		_html.push('</div>');
-		_html.push('</div>');
-        
-	});
+	_html.push('<div class="box col-md-3 col-lg-3">');
+	_html.push('<div class="img_box">');
+	_html.push('<img class="img_box_img" src="../../images/road/img/beijing.png" alt="');
+
+	_html.push("北京道路作业项目");
+	_html.push('">');
+	_html.push('<div class="img_box_caption">');
+	_html.push('<h3 class="img_box_caption_h">');
+	_html.push("北京道路作业项目");
+	_html.push('</h3>');
+	_html.push('<p class="img_box_caption_p">');
+	if(true){
+		_html.push('待作业Tips总量 <span class="label label-info">');
+		_html.push('1234');
+		_html.push('</span> 个</br>');
+	}
+	_html.push('项目ID：<span class="label label-info">');
+	_html.push(11);
+	_html.push('</span>');
+	_html.push('</p>');
+	_html.push('<a class="img_box_caption_a" href="project.html?proId=');
+	_html.push(11);
+	_html.push('"></a>');
+	_html.push('</div>');
+	_html.push('</div>');
+	_html.push('<div class="img_box_title">');
+	_html.push("北京道路作业项目");
+	_html.push('</div>');
+	_html.push('</div>');
+
+
+	_html.push('<div class="box col-md-3 col-lg-3">');
+	_html.push('<div class="img_box">');
+	_html.push('<img class="img_box_img" src="../../images/road/img/beijing.png" alt="');
+
+	_html.push("北京POI作业项目");
+	_html.push('">');
+	_html.push('<div class="img_box_caption">');
+	_html.push('<h3 class="img_box_caption_h">');
+	_html.push("北京POI作业项目");
+	_html.push('</h3>');
+	_html.push('<p class="img_box_caption_p">');
+	if(true){
+		_html.push('待作业Poi总量 <span class="label label-info">');
+		_html.push('1234');
+		_html.push('</span> 个</br>');
+	}
+	_html.push('项目ID：<span class="label label-info">');
+	_html.push(11);
+	_html.push('</span>');
+	_html.push('</p>');
+	_html.push('<a class="img_box_caption_a" href="project.html?access_token='+accessToken);
+	_html.push(11);
+	_html.push('"></a>');
+	_html.push('</div>');
+	_html.push('</div>');
+	_html.push('<div class="img_box_title">');
+	_html.push("北京POI作业项目");
+	_html.push('</div>');
+	_html.push('</div>');
+
 	$(".project-list").html(_html.join(''));
 }
