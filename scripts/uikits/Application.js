a87cc4cf77839795e491c887e53c4daafe98cdcb
wangmingdong <wangmingdong@navinfo.com>
2016-05-17 09:24:16 +0800

    更新token

--- scripts/uikits/Application.js
+++ scripts/uikits/Application.js
@@ -4,7 +4,7 @@ var App = {};
 // web app全局配置信息
 App.Config = {
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "0000029900O7ARFD80913C0339731C51D0F2F9C40F6F983D",
+    accessToken: "0000029900O7CMD9032F0AF65935A3C83CB72E452927E391",
     hbaseServiceUrl: "http://fastmap.navinfo.com/fos/datum",
     resourceUrl: "http://192.168.4.189/resources"
 };
