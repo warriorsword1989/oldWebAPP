b769b0e4aa14e68f3d833cab8f8a00aee9f9b9c6
maliNavinfo@navinfo.com <maliNavinfo@navinfo.com>
2016-05-23 18:28:48 +0800

    充电桩checkbox 显示保存修改

--- scripts/components/poi/ctrls/attr-deep/chargingPoleCtl.js
+++ scripts/components/poi/ctrls/attr-deep/chargingPoleCtl.js
@@ -29,43 +29,10 @@ angular.module('app').controller('chargingPoleCtl', function($scope) {
             }
         }
     };
-    $scope.initCheckbox = function(valueStr, retObj) {
-        var tmp = valueStr ? valueStr.split("|") : [];
-        for (var i = 0; i < tmp.length; i++) {
-            retObj[tmp[i]] = true;
-        }
-    };
-//    $scope.paymentArray = $scope.poi.chargingPole[0].paymentArray;
+    $scope.chargingPlugType = FM.dataApi.Constant.CHARGINGPOLE_PLUGTYPE;
+    $scope.chargingOpenType = FM.dataApi.Constant.CHARGINGPOLE_OPENTYPE;
+    $scope.chargingPayment = FM.dataApi.Constant.CHARGINGPOLE_PAYMENT;
     $scope.charging = $scope.poi.chargingPole[0];
-//    $scope.paymentArr = [
-//         {id:"0",value:'其他',check:false},
-//         {id:"1",value:'现金',check:false},
-//         {id:"2",value:'信用卡',check:false},
-//         {id:"3",value:'借记卡',check:false},
-//         {id:"4",value:'特制充值卡',check:false},
-//         {id:"5",value:'APP',check:false}
-//         ];
-//    if($scope.charging.payment.indexOf('|') > 0){
-//    	var pDefaulArr = $scope.charging.payment.split('|');
-//    	for(var i = 0;i<pDefaulArr.length;i++){
-//    		for(var j = 0; j<$scope.paymentArr.length; j++){
-//    			if(pDefaulArr[i] == $scope.paymentArr[j].id){
-//    				$scope.paymentArr[j].check = true;
-//    				break;
-//    			}
-//    		}
-//    	}
-//    }else{
-//    	for(var j = 0; j<$scope.paymentArr.length; j++){
-//			if($scope.charging.payment == $scope.paymentArr[j].id){
-//				$scope.paymentArr[j].check = true;
-//				break;
-//			}
-//		}
-//    }
-    $scope.initCheckbox($scope.charging.openType, $scope.charging.openType = {});
-    $scope.initCheckbox($scope.charging.plugType, $scope.charging.plugType = {});
-    $scope.initCheckbox($scope.charging.payment, $scope.charging.payment = {});
     //查询充电桩品牌列表
     $scope.$on("loaded",function (event, data) {
         FM.dataApi.ajax.get("charge/row_edit/queryChain/", {
@@ -83,9 +50,6 @@ angular.module('app').controller('chargingPoleCtl', function($scope) {
             }
             console.log(chargeChain);
         });
-    });
-    $scope.$on("save", function(event, data) {
-        $scope.$emit("saveMe", "deepInfo");
     });
     
 });
