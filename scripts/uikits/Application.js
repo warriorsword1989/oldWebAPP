1f2e7163d11ba9e94e1537483d50c842dfc2974d
chenxiao <chenxiaoxiao@navinfo.com>
2016-05-24 09:19:02 +0800

    修改POI黑色版样式

--- scripts/uikits/Application.js
+++ scripts/uikits/Application.js
@@ -4,7 +4,7 @@ var App = {};
 // web app全局配置信息
 App.Config = {
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "000002TJ00O7I7L9595F88241ECE23B1B15841E35E6DB96D",
+    accessToken: "0000029900O7NQJ72AC43A5D617E15CFB2F66C6C5464C055",
     hbaseServiceUrl: "http://fastmap.navinfo.com/fos/datum",
     resourceUrl: "http://192.168.4.189/resources",
     appType: "WEB"
