f1961d41ba247893cd4b860506168385546256b8
liuzhaoxia <liuzhaoxia@navinfo.com>
2016-05-30 10:52:11 +0800

    画线提示

--- scripts/components/road/ctrls/toolBar_cru_ctrl/addShapeCtrl.js
+++ scripts/components/road/ctrls/toolBar_cru_ctrl/addShapeCtrl.js
@@ -478,7 +478,7 @@ addShapeApp.controller("addShapeController", ['$scope', '$ocLazyLoad', function 
                 tooltipsCtrl.setEditEventType(fastmap.mapApi.ShapeOptionType.DRAWPATH);
                 tooltipsCtrl.setCurrentTooltip('开始画线！');
                 tooltipsCtrl.setStyleTooltip("color:black;");
-                tooltipsCtrl.setChangeInnerHtml("点击最后一个点结束画线!");
+                tooltipsCtrl.setChangeInnerHtml("双击最后一个点结束画线!");
                 tooltipsCtrl.setDbClickChangeInnerHtml("点击空格保存画线,或者按ESC键取消!");
             }
             else if (type === "RDSPEEDLIMIT") {
