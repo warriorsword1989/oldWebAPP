76c174ed51d47578c12c0692168cfc696c7600f1
chenxiao <chenxiaoxiao@navinfo.com>
2016-05-30 11:42:12 +0800

    修改poiEditor页面报错问题

--- scripts/uikits/Application.js
+++ scripts/uikits/Application.js
@@ -4,7 +4,7 @@ var App = {};
 // web app全局配置信息
 App.Config = {
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "0000029900O7V5EO7E60400E0C20901FA3153BE109D681DB",
+    accessToken: "0000029900O80TDDF069AAF3FF8E14C6A46BF2A72048759E",
     hbaseServiceUrl: "http://fastmap.navinfo.com/fos/datum",
     resourceUrl: "http://192.168.4.189/resources",
     appType: "WEB"
