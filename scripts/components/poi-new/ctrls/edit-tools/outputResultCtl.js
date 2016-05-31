c0c9076125b7357a4b001f3d607cef4bff128925
wangmingdong <wangmingdong@navinfo.com>
2016-05-31 19:55:20 +0800

    输出结果和检查结果提交

--- scripts/components/poi-new/ctrls/edit-tools/outputResultCtl.js
+++ scripts/components/poi-new/ctrls/edit-tools/outputResultCtl.js
@@ -1,6 +1,6 @@
 angular.module('app').controller('OutputResultCtl', ['$scope', function($scope) {
 
-    $scope.theadInfo = ['序号','规则编码','错误描述','操作','关联POI'];
+    $scope.theadInfo = ['操作','类型','编号'];
     /*检查结果忽略操作*/
     $scope.ignoreCheckResult = function(item){
         $scope.$emit('ignoreItem',item);
