03dba557733e8b782b96ad19a04d64df710072da
liuyang <liuyang4666@navinfo.com>
2016-06-06 16:45:40 +0800

    poiMap

--- apps/poibase/Application.js
+++ apps/poibase/Application.js
@@ -5,7 +5,7 @@ var App = {};
 App.Config = {
     appType: "WEB",
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "0000029900O883GCBCFD0F74CCA3F955D06A0216D31714E7",
+    accessToken: "000002TJ00O8DOLFE61C96480C0918E9AB5DC6A515238A02",
     hbaseServiceUrl: "http://fastmap.navinfo.com/fos/datum",
     resourceUrl: "http://192.168.4.189/resources",
     generalUrl: 'http://192.168.4.188/service',
