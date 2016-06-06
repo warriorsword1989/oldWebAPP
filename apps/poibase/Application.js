68f353737134b76c0099a7e71d1a9adfc3620077
wangmingdong <wangmingdong@navinfo.com>
2016-06-06 14:36:51 +0800

    token

--- apps/poibase/Application.js
+++ apps/poibase/Application.js
@@ -5,7 +5,7 @@ var App = {};
 App.Config = {
     appType: "WEB",
     serviceUrl: "http://192.168.4.189/fos",
-    accessToken: "0000029900O883GCBCFD0F74CCA3F955D06A0216D31714E7",
+    accessToken: "0000029900O8DO7P5E49D93465DA9FFB1D8B981436931A78",
     hbaseServiceUrl: "http://fastmap.navinfo.com/fos/datum",
     resourceUrl: "http://192.168.4.189/resources",
     generalUrl: 'http://192.168.4.188/service',
