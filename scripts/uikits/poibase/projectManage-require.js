bb7831e6d7f8ffe93931777b3fceef0815b1831a
chenxiao <chenxiaoxiao@navinfo.com>
2016-06-16 11:07:48 +0800

    删除app-util.js相关内容

--- scripts/uikits/poibase/projectManage-require.js
+++ scripts/uikits/poibase/projectManage-require.js
@@ -5,11 +5,10 @@ requirejs.config({
     baseUrl: '../../scripts/',
     paths: {
         'angular': 'libs/angularjs/1.4.4/angular',
-        'ngCookies':'libs/angularjs/1.4.4/angular-cookies.min',
+        'ngCookies': 'libs/angularjs/1.4.4/angular-cookies.min',
         'ocLazyLoad': 'libs/ocLazyLoad/ocLazyLoad.require',
         'uiBootstrap': 'libs/bootstrap-3.3.5/js/ui-bootstrap-tpls-1.3.2',
         'application': 'uikits/Application',
-        'appUtil': 'uikits/common/app-util',
         'projectManageCtl': 'uikits/poibase/projectManageCtl',
         'fastmap': 'fastmap/fastmap',
         'fm-util': 'fastmap/fastmap-util',
@@ -17,23 +16,24 @@ requirejs.config({
         'dataService': 'uikits/poibase/dataService',
         'poiService': 'uikits/poibase/dataService-poi',
         'metaService': 'uikits/poibase/dataService-meta',
-        'ngTable':'libs/ng-table/ng-table',
-        'ngSanitize':'libs/angularjs/1.4.4/angular-sanitize'
+        'ngTable': 'libs/ng-table/ng-table',
+        'ngSanitize': 'libs/angularjs/1.4.4/angular-sanitize'
     },
     shim: {
-        'angular':{exports:'angular'},                   //ngTable需要用angular的名字;
+        'angular': {
+            exports: 'angular'
+        }, //ngTable需要用angular的名字;
         'ngCookies': ['angular'],
         'ocLazyLoad': ['angular'],
         'uiBootstrap': ['angular'],
-        'dataService':['angular'],
+        'dataService': ['angular'],
         'poiService': ['dataService'],
         'metaService': ['dataService'],
         'fastmap': ['application'],
-        'appUtil': ['application'],
-        'select2':['jquery'],
-        'ngTable':['angular'],
-        'ngSanitize':['angular'],
-        'projectManageCtl': ['ocLazyLoad' ,'uiBootstrap', 'application', 'appUtil', 'poiService','metaService','ngTable','ngSanitize','ngCookies']
+        'select2': ['jquery'],
+        'ngTable': ['angular'],
+        'ngSanitize': ['angular'],
+        'projectManageCtl': ['ocLazyLoad', 'uiBootstrap', 'application', 'poiService', 'metaService', 'ngTable', 'ngSanitize', 'ngCookies']
     }
 });
 // Start the main app logic.
