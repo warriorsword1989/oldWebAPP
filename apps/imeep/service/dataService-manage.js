e766fcb871e92e6c3fe450749f659ddd62e7dc49
warriorsword1989 <linglong@navinfo.com>
2016-06-17 15:50:35 +0800

    完善任务选择

---
+++ apps/imeep/service/dataService-manage.js
@@ -0,0 +1,19 @@
+angular.module("dataService").service("mPoi", ["$http", "$q", "ajax", function($http, $q, ajax) {
+    //根据用户名查找子任务列表;
+    this.querySubtaskByUser = function(paramObj){
+        var defer = $q.defer();
+        ajax.get("man/subtask/listByUser", {
+            parameter:JSON.stringify(paramObj),
+            urlType:'general'
+        }).success(function(data){
+            if(data.errcode == 0){
+                defer.resolve(data.data.result);
+            }else{
+                defer.resolve("查询子任务列表出错：" +data.errmsg);
+            }
+        }).error(function(rejection) {
+            defer.reject(rejection);
+        });
+        return defer.promise;
+    }
+}]);
