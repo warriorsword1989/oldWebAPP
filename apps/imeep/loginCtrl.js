0c67c6c8bc0892d06a68d9cd4695117f765a68aa
chenxiao <chenxiaoxiao@navinfo.com>
2016-06-27 17:46:15 +0800

    登陆任务选择功能优化

--- apps/imeep/loginCtrl.js
+++ apps/imeep/loginCtrl.js
@@ -1,31 +1,18 @@
 /**
  * Created by Administrator on 2016/5/4.
  */
-angular.module('loginApp', ['fastmap.uikit','ngCookies']).controller('loginCtrl', function($scope, $http, $cookies) {
-    $scope.$on("startLogin", function(event, data) {
-        //请求登录接口;
-        $http({
-            method: 'GET',
-            url: App.Util.getFullUrl('man/userInfo/login/'),
-            params: {
-                parameter:{
-                    "userNickName": data.userName,
-                    "userPassword": data.password
+angular.module('loginApp', ['fastmap.uikit', 'ngCookies', 'dataService']).controller('loginCtrl', ["$scope", "$cookies", "dsManage",
+    function($scope, $cookies, dsManage) {
+        $scope.$on("startLogin", function(event, data) {
+            dsManage.login(data.userName, data.password).then(function(rest) {
+                if (rest) {
+                    if (rest.access_token) {
+                        window.location.href = "./task/taskSelection.html?access_token=" + rest.access_token;
+                    } else {
+                        swal("登陆出错", data, "error");
+                    }
                 }
-            }
-        }).success(function(data) {
-            if (data.errcode == 0) {
-                if (data.data.access_token) {
-                    //$cookies.put('FM_USER_ID',data.data.userId);
-                    //$cookies.put('FM_USER_NAME',data.data.realName);
-                    //$cookies.put('FM_USER_TYPE',data.data.userType);
-                    //$cookies.put('FM_USER_ROLES',data.data.roleCode.join(","));
-                    //$cookies.put('FM_USER_GROUPS',(data.data.userGroups ? data.data.userGroups : []).join(","));
-                    window.location.href = "./task/taskSelection.html?access_token=" + data.data.access_token;
-                }
-            } else{
-                swal("登陆出错", data.errmsg, "error");
-            }
-        })
-    });
-});
+            });
+        });
+    }
+]);
