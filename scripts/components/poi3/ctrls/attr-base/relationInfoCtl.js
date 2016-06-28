124e501a5163cb3d5242fcc5a37167fd5cde2d82
wuzhen <wuzhen@navinfo.com>
2016-06-28 20:44:54 +0800

    修改父子关系，电话保存，POI保存接口

--- scripts/components/poi3/ctrls/attr-base/relationInfoCtl.js
+++ scripts/components/poi3/ctrls/attr-base/relationInfoCtl.js
@@ -1,4 +1,6 @@
-angular.module('app').controller('relationInfoCtl', ['$scope', function($scope) {
+angular.module('app').controller('relationInfoCtl', ['$scope','dsEdit', function($scope,dsEdit) {
+    var objCtrl = fastmap.uikit.ObjectEditController();
+
 
     $scope.showChildrenPoisInMap = function (pid){
         $scope.$emit('emitChildren', pid);
@@ -7,4 +9,30 @@ angular.module('app').controller('relationInfoCtl', ['$scope', function($scope) 
         $scope.$emit('emitParent', pid);
     };
 
+    $scope.deleteParent = function (poi){
+        var pid = poi.parents[0].parentPoiPid;
+        var poiId = poi.pid;
+        dsEdit.deleteParent(poiId).then(function (data) {
+            if(data){
+                dsEdit.getByPid(poiId, "IXPOI").then(function (rest) {
+                    if (rest) {
+                        objCtrl.setCurrentObject('IXPOI', rest);
+                        objCtrl.setOriginalData(objCtrl.data.getIntegrate());
+                        //$scope.$emit("SWITCHCONTAINERSTATE", {});
+                        // $scope.$emit("transitCtrlAndTpl", {
+                        //     "loadType": "tipsTplContainer",
+                        //     "propertyCtrl": appPath.poi + "ctrls/attr-tips/poiPopoverTipsCtl",
+                        //     "propertyHtml": appPath.root + appPath.poi + "tpls/attr-tips/poiPopoverTips.html"
+                        // });
+                        // $scope.$emit("transitCtrlAndTpl", {
+                        //     "loadType": "attrTplContainer",
+                        //     "propertyCtrl": appPath.poi + "ctrls/attr-base/generalBaseCtl",
+                        //     "propertyHtml": appPath.root + appPath.poi + "tpls/attr-base/generalBaseTpl.html"
+                        // });
+                    }
+                });
+            }
+        });
+
+    }
 }]);
