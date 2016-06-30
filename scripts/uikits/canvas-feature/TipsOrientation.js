bff1a03b0d7b7329c4452317feb8f2bc1963ade8
wangmingdong <wangmingdong@navinfo.com>
2016-06-30 11:29:00 +0800

    方向看板大小调整

--- scripts/uikits/canvas-feature/TipsOrientation.js
+++ scripts/uikits/canvas-feature/TipsOrientation.js
@@ -1,30 +1,14 @@
 fastmap.uikit.canvasTips.TipsOrientation = fastmap.uikit.canvasTips.Tips.extend({
     setAttribute: function (item) {
         this.geometry['coordinates'] = item.g;
-        if(item.m.a == "0"){
-            this.properties['markerStyle']["icon"].push(
-                fastmap.uikit.canvasTips.Tips.getIconStyle({
-                    iconName: '../../../images/road/tips/1401/1401_0_0.svg',
-                    row: 0,
-                    column: 1,
-                    location: this.geometry['coordinates'],
-                    fillStyle:this.redFill,
-                    scalex:5,
-                    scaley:5
-                })
-            );
-        } else {
-            this.properties['markerStyle']["icon"].push(
-                fastmap.uikit.canvasTips.Tips.getIconStyle({
-                    iconName: '../../../images/road/tips/1401/1401_0_0.svg',
-                    row: 0,
-                    column: 1,
-                    location: this.geometry['coordinates'],
-                    fillStyle:this.greenFill,
-                    scalex:5,
-                    scaley:5
-                })
-            );
-        }
+        this.properties['markerStyle']["icon"].push(
+            fastmap.uikit.canvasTips.Tips.getIconStyle({
+                iconName: '../../../images/road/tips/1401/1401_0_0.svg',
+                row: 0,
+                column: 1,
+                location: this.geometry['coordinates'],
+                fillStyle:item.m.a == "0"?this.redFill:this.greenFill,
+            })
+        );
     }
 });
