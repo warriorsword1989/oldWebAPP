a638019fe74b8d3751d2cd0bda360d61a3642d5f
liuyang <liuyang4666@navinfo.com>
2016-07-29 14:32:25 +0800

    修改bug

--- scripts/components/tools/ctrls/toolbar-map/selectShapeCtrl.js
+++ scripts/components/tools/ctrls/toolbar-map/selectShapeCtrl.js
@@ -1303,6 +1303,7 @@ angular.module("app").controller("selectShapeCtrl", ["$scope", '$ocLazyLoad', '$
                             }
                         })
                     });
+                    return;
                 } else if (type === "MODIFYOUTLINE") {
                     map.currentTool = new fastmap.uikit.SelectPath({
                         map: map,
