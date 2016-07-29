bad6eb03f6a8285a521ee7522e8bedca6fccb40d
maliNavinfo@navinfo.com <maliNavinfo@navinfo.com>
2016-07-29 14:51:12 +0800

    gate

--- scripts/dataApi/canvasFeatures/RdGate.js
+++ scripts/dataApi/canvasFeatures/RdGate.js
@@ -3,7 +3,7 @@ fastmap.uikit.canvasFeature.RdGate = fastmap.uikit.canvasFeature.Feature.extend(
         this.geometry['type'] = 'Point';
         this.properties['markerStyle'] = {};
         this.properties['markerStyle']["icon"] = [];
-        this.properties["featType"] = "RDSLOPE";
+        this.properties["featType"] = "RDGATE";
         this.properties['markerStyle']["icon"].push(
             fastmap.uikit.canvasTips.Tips.getIconStyle({
                 iconName: '../../../images/road/rdGate/' + parseInt(item.m.a) + '.svg',
