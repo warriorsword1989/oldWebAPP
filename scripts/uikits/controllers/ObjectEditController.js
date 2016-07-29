3e9b4796e4c0f4e81e6df25de0353332228ae841
wuzhen <wuzhen@navinfo.com>
2016-07-29 16:11:20 +0800

    增加对比较方法中数组的控制

--- scripts/uikits/controllers/ObjectEditController.js
+++ scripts/uikits/controllers/ObjectEditController.js
@@ -309,7 +309,9 @@ fastmap.uikit.ObjectEditController = (function() {
                             for (var k = 0, lenK = oriData[item].length; k < lenK; k++) {
                                 if (indexOfData[oriData[item][k][key]]) {
                                     var obj = this.compareJson(pids,oriData[item][k], data[item][indexOfData[oriData[item][k][key]]["index"]], "UPDATE");
-                                    objArr.push(obj);
+                                    if (obj) {
+                                        objArr.push(obj);
+                                    }
                                 } else {
                                     obj = oriData[item][k];
                                     obj["objStatus"] = "DELETE";
