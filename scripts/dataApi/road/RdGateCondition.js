6dec8fd2a1638cdf2b2153ccb55fe0f174fce458
maliNavinfo@navinfo.com <maliNavinfo@navinfo.com>
2016-08-01 18:04:48 +0800

    gate

---
+++ scripts/dataApi/road/RdGateCondition.js
@@ -0,0 +1,44 @@
+/**
+ * Created by mali on 2016/8/1.
+ * Class RdGateCondition
+ */
+
+fastmap.dataApi.RdGateCondition = fastmap.dataApi.GeoDataModel.extend({
+
+    initialize: function (data, options) {
+        L.setOptions(this, options);
+        this.geoLiveType = "RDGATECONDITION";
+        this.setAttributeData(data);
+    },
+
+    setAttributeData:function(data){
+    	var data = {};
+    	this.pid = data["pid"];
+        this.validObj= data["validObj"] || 0;
+        this.timeDomain = data["timeDomain"] || "";
+
+    },
+
+    getSnapShot:function() {
+        var data = {};
+        data["pid"] = this.pid;
+        data["validObj"] = this.validObj;
+        data["timeDomain"] = this.timeDomain;
+        data["geoLiveType"] = this.geoLiveType;
+        return data;
+    },
+
+    getIntegrate:function() {
+        var data = {};
+        data["pid"] = this.pid;
+        data["validObj"] = this.validObj;
+        data["timeDomain"] = this.timeDomain;
+        data["geoLiveType"] = this.geoLiveType;
+        return data;
+    }
+});
+
+fastmap.dataApi.rdGateCondition = function (data, options) {
+    return new fastmap.dataApi.RdGateCondition(data, options);
+};
+
