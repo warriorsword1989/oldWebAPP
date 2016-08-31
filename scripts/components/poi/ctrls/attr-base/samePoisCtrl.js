723d1dcb3d760648284c6a4155b2cb71c31518be
liuyang <liuyang4666@navinfo.com>
2016-08-31 15:27:48 +0800

    同一关系样式修改

--- scripts/components/poi/ctrls/attr-base/samePoisCtrl.js
+++ scripts/components/poi/ctrls/attr-base/samePoisCtrl.js
@@ -1,36 +1,26 @@
-angular.module('app').controller('samePoisCtrl', ['$scope','dsEdit', function($scope,dsEdit) {
+angular.module('app').controller('samePoisCtrl', ['$scope', 'dsEdit', function ($scope, dsEdit) {
     var objCtrl = fastmap.uikit.ObjectEditController();
     var highRenderCtrl = fastmap.uikit.HighRenderController();
+    var layerCtrl = fastmap.uikit.LayerController();
+    var poiLayer = layerCtrl.getLayerById('poi');
 
-    $scope.initializeData = function(){
-        $scope.slopeData = objCtrl.data;
-       
-    };
-    $scope.initializeData();
-    $scope.deleteSamePoi = function (poi){
-        var pid = poi.parents[0].parentPoiPid;
-        var poiId = poi.pid;
-        dsEdit.deleteParent(poiId).then(function (data) {
-            if(data){
-                dsEdit.getByPid(poiId, "IXPOI").then(function (rest) {
+    $scope.deleteSamePoi = function (poi) {
+        var param = {};
+        param["command"] = "DELETE";
+        param["dbId"] = App.Temp.dbId;
+        param["type"] = "IXSAMEPOI";
+        param["objId"] = poi.samepoiParts[0].groupId;
+        dsEdit.save(param).then(function (data) {
+            if (data != null) {
+                poiLayer.redraw();
+                swal("操作成功", "删除POI同一关系成功", "success");
+                dsEdit.getByPid(poi.pid, "IXPOI").then(function (rest) {
                     if (rest) {
                         objCtrl.setCurrentObject('IXPOI', rest);
                         objCtrl.setOriginalData(objCtrl.data.getIntegrate());
-                        //$scope.$emit("SWITCHCONTAINERSTATE", {});
-                        // $scope.$emit("transitCtrlAndTpl", {
-                        //     "loadType": "tipsTplContainer",
-                        //     "propertyCtrl": appPath.poi + "ctrls/attr-tips/poiPopoverTipsCtl",
-                        //     "propertyHtml": appPath.root + appPath.poi + "tpls/attr-tips/poiPopoverTips.html"
-                        // });
-                        // $scope.$emit("transitCtrlAndTpl", {
-                        //     "loadType": "attrTplContainer",
-                        //     "propertyCtrl": appPath.poi + "ctrls/attr-base/generalBaseCtl",
-                        //     "propertyHtml": appPath.root + appPath.poi + "tpls/attr-base/generalBaseTpl.html"
-                        // });
                     }
                 });
             }
         });
-
     }
 }]);
