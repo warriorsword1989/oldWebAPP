0b221e812d411677634a69d322755adfcd7cbc3b
wangmingdong <wangmingdong@navinfo.com>
2016-09-12 21:08:43 +0800

    hotKeyEvent冲突

--- apps/imeep/editor/hotKeyEvent.js
+++ apps/imeep/editor/hotKeyEvent.js
@@ -1334,15 +1334,15 @@ function bindHotKeys(ocLazyLoad, scope, dsEdit, appPath) {
                     //获取当前的ctrl和tpl的对象
                     highRenderCtrl._cleanHighLight();
                     highRenderCtrl.highLightFeatures.length = 0;
+                    for(var i=0,len=featCodeCtrl.getFeatCode().links.length;i<len;i++){
+                      featCodeCtrl.getFeatCode().links[i] = parseInt(featCodeCtrl.getFeatCode().links[i]);
+                    }
                     objEditCtrl.memo = featCodeCtrl.getFeatCode();
                     objEditCtrl.setCurrentObject('RDLANE', {
                       linkPids:featCodeCtrl.getFeatCode().links,
                       laneDir:featCodeCtrl.getFeatCode().laneDir,
                       laneInfos:data.data
                     });
-                    // ocLazyLoad.load(appPath.road + "ctrls/attr_lane_ctrl/rdLaneCtrl").then(function () {
-                    // 	scope.attrTplContainer = appPath.root + appPath.road + "tpls/attr_lane_tpl/rdLaneTpl.html";
-                    // });
                     var showLaneInfoObj = { //这样写的目的是为了解决子ctrl只在第一次加载时执行的问题,解决的办法是每次点击都加载一个空的ctrl，然后在加载namesOfDetailCtrl。
                        "loadType": "attrTplContainer",
                        "propertyCtrl": 'scripts/components/road/ctrls/blank_ctrl/blankCtrl',
@@ -1357,11 +1357,6 @@ function bindHotKeys(ocLazyLoad, scope, dsEdit, appPath) {
                        }
                    };
                    scope.$emit("transitCtrlAndTpl", showLaneInfoObj);
-                    // scope.$emit("SWITCHCONTAINERSTATE", {
-                    //     "attrContainerTpl": true,
-                    //     "subAttrContainerTpl": false
-                    // });
-                    // $scope.attrTplContainerSwitch(true);
                   });
             } else if (shapeCtrl.editType === "rdLaneTopoDetail") {    //查询详细车道
                 var param = {
