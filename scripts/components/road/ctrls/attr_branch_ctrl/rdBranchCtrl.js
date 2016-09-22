00157e464fb87e6336cd4c77dc2f6dcd530be2a0
wangmingdong <wangmingdong@navinfo.com>
2016-09-22 10:49:50 +0800

    分歧箭头图校验

--- scripts/components/road/ctrls/attr_branch_ctrl/rdBranchCtrl.js
+++ scripts/components/road/ctrls/attr_branch_ctrl/rdBranchCtrl.js
@@ -87,6 +87,8 @@ namesOfBranch.controller("namesOfBranchCtrl",['$scope','$timeout','$ocLazyLoad',
                 return new RegExp('^[0-2]*$').test($scope.diverObj.details[0].arrowCode.substr(0,1));
             case 1:
                 return new RegExp('^[0-2]*$').test($scope.diverObj.details[0].arrowCode.substr(0,1));
+            case 2:
+                return new RegExp('^[0-2]*$').test($scope.diverObj.details[0].arrowCode.substr(0,1));
             case 3:
                 return new RegExp('^[ec]').test($scope.diverObj.details[0].arrowCode);
             case 4:
