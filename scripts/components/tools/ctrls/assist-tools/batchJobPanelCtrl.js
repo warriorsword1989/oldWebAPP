1aa58806ba70f96782b3342e5a06e2f7a10d0354
warriorsword1989 <linglong@navinfo.com>
2016-09-27 18:52:01 +0800

    中等级bug修改

--- scripts/components/tools/ctrls/assist-tools/batchJobPanelCtrl.js
+++ scripts/components/tools/ctrls/assist-tools/batchJobPanelCtrl.js
@@ -6,20 +6,42 @@ angular.module('app').controller("BatchJobPanelCtrl", ['$scope', '$interval', 'd
         $scope.batchBoxData = [];
         $scope.currentBatchItems = [];
         $scope.selectedBatches = {};
-        $scope.currentPage = 1;
+        $scope.currentPaging = 1;
+        $scope.batchType=1;
+        $scope.pageSize = 1;
+
+
+
         //获取所有批处理包;
         function getBatchBox(){
-            dsEdit.batchBox("batchbag.json").then(function(data){
+            var param = {
+                pageNumber:$scope.pageSize,
+                currentPage:$scope.currentPaging,
+                batchType:$scope.batchType
+            };
+            dsEdit.batchBox(param).then(function(data){
                 $scope.batchBoxData = data;
+                $scope.totalNum = data[0].total;
             });
         }
+
+        //切换道路和poi批处理tab页;
+        $scope.switchBatchType = function(type){
+            $scope.batchType = type;
+            getBatchBox();
+        }
+
+        $scope.pageChanged = function(){
+            getBatchBox($scope.currentPaging);
+        }
+
         //点击table行查询当前批处理包下的批处理规则;
         $scope.getBatchItem = function(param){
-            dsEdit.batchBox('batchItem.json').then(function(res){
-                $scope.currentBatchItems = res[parseInt(param.id)-1].data;
-                $scope.batchSelect(param);
-            })
+            $scope.currentBatchItems = param.rules;
+            $scope.batchSelect(param);
         }
+
+
         //全选或反选处理;
         $scope.batchSelect = function(param){
             if(param.checked){
@@ -33,42 +55,41 @@ angular.module('app').controller("BatchJobPanelCtrl", ['$scope', '$interval', 'd
             }
         }
 
-        $scope.selectedBatches = [1];
 
-        $scope.running = false;
-        $scope.progress = 0;
-        $scope.doExecute = function() {
-            var batches = [];
-            for (var key in $scope.selectedBatches) {
-                if ($scope.selectedBatches[key]) {
-                    batches.push(key);
-                }
-            }
-            if (batches.length == 0) {
-                swal("请选择要执行的批处理", "", "info");
-                return;
-            } else {
-                $scope.running = true;
-                $scope.$emit("job-batch", {
-                    status: 'begin'
-                });
-                swal("自动录入服务启动成功（模拟运行，服务正在调试中）！", "", "success");
-                $scope.progress = 0;
-                var loop = $interval(function() {
-                    $scope.progress += 20;
-                    if ($scope.progress == 100) {
-                        clearInterval(loop);
-                        $scope.running = false;
-                        $scope.$emit("job-batch", {
-                            status: 'end'
-                        });
-                        // swal("自动录入服务模拟运行完成！", "", "success");
-                    }
-                }, 1000)
-                return;
-            }
-        };
+        //$scope.running = false;
+        //$scope.progress = 0;
 
+        //$scope.doExecute = function() {
+        //    var batches = [];
+        //    for (var key in $scope.selectedBatches) {
+        //        if ($scope.selectedBatches[key]) {
+        //            batches.push(key);
+        //        }
+        //    }
+        //    if (batches.length == 0) {
+        //        swal("请选择要执行的批处理", "", "info");
+        //        return;
+        //    } else {
+        //        $scope.running = true;
+        //        $scope.$emit("job-batch", {
+        //            status: 'begin'
+        //        });
+        //        swal("自动录入服务启动成功（模拟运行，服务正在调试中）！", "", "success");
+        //        $scope.progress = 0;
+        //        var loop = $interval(function() {
+        //            $scope.progress += 20;
+        //            if ($scope.progress == 100) {
+        //                clearInterval(loop);
+        //                $scope.running = false;
+        //                $scope.$emit("job-batch", {
+        //                    status: 'end'
+        //                });
+        //                // swal("自动录入服务模拟运行完成！", "", "success");
+        //            }
+        //        }, 1000)
+        //        return;
+        //    }
+        //};
 
         getBatchBox()
 
