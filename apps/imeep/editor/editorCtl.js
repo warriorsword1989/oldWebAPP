fe5c597bfc522b7fef4bc460a95f6692ce5fd90f
chenxiao <chenxiaoxiao@navinfo.com>
2016-09-30 14:26:06 +0800

    工具栏改版（只有显示，未关联功能）

--- apps/imeep/editor/editorCtl.js
+++ apps/imeep/editor/editorCtl.js
@@ -18,7 +18,7 @@ angular.module('app', ['oc.lazyLoad', 'fastmap.uikit', 'ui.layout', 'ngTable', '
 		$scope.metaData = {}; //存放元数据
 		$scope.metaData.kindFormat = {}, $scope.metaData.kindList = [], $scope.metaData.allChain = {} , $scope.topKind = {} , $scope.mediumKind = {} ;
 		$scope.metaData.kindFormatPart = {},  $scope.metaData.kindListPart = [];
-		$scope.showLoading = true;
+		// $scope.showLoading = true;
 		$scope.showTab = true;
 		$scope.selectedTool = 1;
 		$scope.dataListType = 1;
@@ -37,7 +37,7 @@ angular.module('app', ['oc.lazyLoad', 'fastmap.uikit', 'ui.layout', 'ngTable', '
 		// $scope.outputResult = []; //输出结果
 		/*切换项目平台*/
 		$scope.changeProject = function (type) {
-			$scope.showLoading = true;
+			// $scope.showLoading = true;
 			$scope.showPopoverTips = false;
 			$scope.tipsPanelOpened = false;
 			if (type == 1) { //poi
@@ -257,12 +257,12 @@ angular.module('app', ['oc.lazyLoad', 'fastmap.uikit', 'ui.layout', 'ngTable', '
 			}
 		};
 
-		var loadToolsPanel = function (callback) {
+		var loadToolsPanel = function () {
+			$ocLazyLoad.load(appPath.root + 'scripts/components/tools/ctrls/toolbar-map/editorToolbarCtrl.js').then(function () {
+				$scope.editorToolbarTpl = appPath.root + 'scripts/components/tools/tpls/toolbar-map/editorToolbarTpl.htm';
+			});
 			$ocLazyLoad.load(appPath.root + 'scripts/components/tools/ctrls/toolbar-map/toolbarCtrl.js').then(function () {
 				$scope.mapToolbar = appPath.root + 'scripts/components/tools/tpls/toolbar-map/toolbarTpl.htm';
-				if (callback) {
-					callback();
-				}
 			});
 			$ocLazyLoad.load(appPath.poi + 'ctrls/edit-tools/optionBarCtl').then(function () {
 				$scope.consoleDeskTpl = appPath.root + appPath.poi + 'tpls/edit-tools/optionBarTpl.html';
@@ -311,14 +311,13 @@ angular.module('app', ['oc.lazyLoad', 'fastmap.uikit', 'ui.layout', 'ngTable', '
 					loadMap(data);
 					var promises = loadMetaData();
 					$q.all(promises).then(function () {
-						loadToolsPanel(function () {
-							if (data.type == 0) { // POI任务
-								$scope.changeProject(1);
-							} else { // 一体化、道路、专项任务
-								$scope.changeProject(2);
-							}
-							bindHotKeys($ocLazyLoad, $scope, dsEdit, appPath); //注册快捷键
-						});
+						loadToolsPanel();
+						if (data.type == 0) { // POI任务
+							$scope.changeProject(1);
+						} else { // 一体化、道路、专项任务
+							$scope.changeProject(2);
+						}
+						bindHotKeys($ocLazyLoad, $scope, dsEdit, appPath); //注册快捷键
 					});
 				}
 			});
