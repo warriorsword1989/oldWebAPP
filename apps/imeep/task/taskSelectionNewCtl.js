9529f0aae86720261f4098497e875b928c42a26c
warriorsword1989 <linglong@navinfo.com>
2016-10-09 20:57:34 +0800

    子任务改版完善

--- apps/imeep/task/taskSelectionNewCtl.js
+++ apps/imeep/task/taskSelectionNewCtl.js
@@ -6,72 +6,46 @@ angular.module('app', ['ui.layout', 'dataService', 'ngCookies','highcharts-ng','
 
         /***********************************控制器初始化以及事件监听绑定***********************************/
         $scope.currentTab = 1;
-        $scope.deepType=''
-        //是否显示精编任务列表（头部控制）;
-        $scope.showDetailEdit = false;
-        //是否显示深度信息任务列表（内容控制）;
-        $scope.isDeepTask = false;
-        //当前高亮的格网数组;
-        $scope.currentHighLight = [];
-        //编辑开关;
-        $scope.startBtnDisabled = true;
-        //顶标签初始状态;
-        $scope.dataListType = 4;
-        //顶标签的当前字符状态;
-        $scope.dataStringType = '';
-        //侧标签初始状态;
-        $scope.taskStatus = 6;
-        //初始默认状态下的请求参数;
-        //$scope.requestParams = {
-        //    classType: 0,
-        //    classStage:1
-        //};
         $scope.myOption = '0';
-        $scope.redUp = 'unactived';
         $scope.tab1Url='../../../images/main/task/icon-c1.png';
         $scope.tab2Url='../../../images/main/task/icon-h2.png';
-        $scope.hhh = function(){
-            $scope.redUp = 'actived';
+
+        $scope.selectArrow = false;
+
+        $scope.condtionChange = function(){
+            $scope.selectArrow = false;
         }
-        $scope.aaaa = function(){
-            $scope.redUp = 'unactived';
+        $scope.changeSelectArrowStyle = function(){
+            $scope.selectArrow = true;
+
         }
-        $scope.go = function(param){
+        $scope.goEditorPage = function(param){
             window.location.href = "../editor/editor.html?access_token=" + App.Temp.accessToken + "&subtaskId=" + param;
         }
-        //当前选中子任务对象;
-        $scope.currentTaskData = null;
-        //是否信息面板;
-        $scope.infoPanelOpened = 'none';
-        //所有的当前高亮grid数据
-        $scope.currentHighligtGrid = [];
 
+        /*切换当前作业和历史作业tab页*/
+        $scope.chnageTab = function(param){
+            $scope.currentTab = param;
+            if($scope.currentTab==1){
+                $scope.tab1Url='../../../images/main/task/icon-c1.png';
+                $scope.tab2Url='../../../images/main/task/icon-h2.png';
+            }else{
+                $scope.tab1Url='../../../images/main/task/icon-c2.png';
+                $scope.tab2Url='../../../images/main/task/icon-h1.png';
+            }
+            loadSubTaskfn();
+        }
 
+        $scope.logout = function(){
+            App.Util.logout();
+        }
 
-        ////获取当前任务作业类型;
-        //$scope.getTaskProgresing = function(){
-        //    if($scope.summary){
-        //        return $scope.summary.percent+'%';
-        //    }
-        //}
         //$scope.getTaskSeason = function(){
         //    if($scope.currentTaskData){
         //        return $scope.currentTaskData.descp;
         //    }
         //}
         //
-        ////拼接开始时间和结束时间的显示方式;
-        //$scope.getDiyDateFormat = function(type){
-        //    var tempTime = null;
-        //    if($scope.currentTaskData){
-        //        if(type==='start'){
-        //            tempTime = $scope.currentTaskData.planStartDate;
-        //        }else{
-        //            tempTime = $scope.currentTaskData.planEndDate;
-        //        }
-        //        return getDateFormat(tempTime);
-        //    }
-        //}
 
         /***********************************控制器私有方法***********************************/
 
@@ -89,30 +63,25 @@ angular.module('app', ['ui.layout', 'dataService', 'ngCookies','highcharts-ng','
                 taskMonth = tempDate.getMonth();
                 taskDay = tempDate.getDate();
             }
-            return taskYear+' . '+taskMonth+' . '+taskDay;
+            return taskYear+'-'+taskMonth+'-'+taskDay;
         }
 
         /*加载子任务列表*/
         function loadSubTaskfn() {
-            //$scope.dataLoaded = false;
-            //if (!obj) return;
             dsManage.getSubtaskListByUser({
                 'exeUserId': $cookies.get('FM_USER_ID'),
-                //'status': 0,
-                //'stage': 1,
-                //'type': 0,
+                'status': $scope.currentTab,
                 'snapshot': 0,
                 'platForm':1,
                 'pageNum': 1,
-                'pageSize': 20
+                'pageSize': 100
             }).then(function(data) {
                 $scope.dataLoaded = true;
-                for(var i=0;i<data.length;i++){
-                    if(!data[i].name){
-                        data[i].name = '（无）';
-                    }
-                }
                 $scope.currentSubTaskList = data;
+                for(var i=0;i<data.length;i++){
+                    $scope.currentSubTaskList[i].planEndDate = getDateFormat($scope.currentSubTaskList[i].planEndDate);
+                    $scope.currentSubTaskList[i].planStartDate = getDateFormat($scope.currentSubTaskList[i].planStartDate);
+                }
                 /*-------------------获取所有子任务的统计数据并重新组织对象------------------*/
                 var currentIndex = 0;
                 function requestfn(){
