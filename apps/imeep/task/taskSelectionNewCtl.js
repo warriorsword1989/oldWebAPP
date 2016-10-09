32ae3edf764eb3b5313da9a6c8bb184015b69464
warriorsword1989 <linglong@navinfo.com>
2016-10-10 00:06:23 +0800

    子任务完善

--- apps/imeep/task/taskSelectionNewCtl.js
+++ apps/imeep/task/taskSelectionNewCtl.js
@@ -4,21 +4,24 @@
 angular.module('app', ['ui.layout', 'dataService', 'ngCookies','highcharts-ng','ui.bootstrap']).controller('TaskSelectionCtlNew', ['$scope', 'dsManage', '$q', '$cookies', '$location','$timeout',
     function($scope, dsManage, $q, $cookies, $location,$timeout) {
 
-        /***********************************控制器初始化以及事件监听绑定***********************************/
         $scope.currentTab = 1;
         $scope.myOption = '0';
         $scope.tab1Url='../../../images/main/task/icon-c1.png';
         $scope.tab2Url='../../../images/main/task/icon-h2.png';
 
         $scope.selectArrow = false;
-
+        $scope.showLoading = true;
         $scope.condtionChange = function(){
+//            loadSubTaskfn();
+        }
+
+        $scope.changeSelectOnFoucs = function(){
+            $scope.selectArrow = true;
+        }
+        $scope.changeSelectOnBlur = function(){
             $scope.selectArrow = false;
         }
-        $scope.changeSelectArrowStyle = function(){
-            $scope.selectArrow = true;
 
-        }
         $scope.goEditorPage = function(param){
             window.location.href = "../editor/editor.html?access_token=" + App.Temp.accessToken + "&subtaskId=" + param;
         }
@@ -40,15 +43,8 @@ angular.module('app', ['ui.layout', 'dataService', 'ngCookies','highcharts-ng','
             App.Util.logout();
         }
 
-        //$scope.getTaskSeason = function(){
-        //    if($scope.currentTaskData){
-        //        return $scope.currentTaskData.descp;
-        //    }
-        //}
-        //
 
         /***********************************控制器私有方法***********************************/
-
         //格式化日期显示;
         function getDateFormat(parmas){
             var taskYear = taskMonth = taskDay = '';
@@ -78,9 +74,26 @@ angular.module('app', ['ui.layout', 'dataService', 'ngCookies','highcharts-ng','
             }).then(function(data) {
                 $scope.dataLoaded = true;
                 $scope.currentSubTaskList = data;
+                $scope.showLoading = false;
                 for(var i=0;i<data.length;i++){
+                    /*格式化返回时间*/
                     $scope.currentSubTaskList[i].planEndDate = getDateFormat($scope.currentSubTaskList[i].planEndDate);
                     $scope.currentSubTaskList[i].planStartDate = getDateFormat($scope.currentSubTaskList[i].planStartDate);
+                    /*格式化子任务类型*/
+                    switch($scope.currentSubTaskList[i].type.toString()){
+                        case '0':$scope.currentSubTaskList[i].type = 'POI';break;
+                        case '1':$scope.currentSubTaskList[i].type = '道路';break;
+                        case '2':$scope.currentSubTaskList[i].type = '一体化';break;
+                        case '3':$scope.currentSubTaskList[i].type = '一体化_GRID粗编';break;
+                        case '4':$scope.currentSubTaskList[i].type = '一体化_区域粗编';break;
+                        case '5':$scope.currentSubTaskList[i].type = '多源POI';break;
+                        case '6':$scope.currentSubTaskList[i].type = '代理店';break;
+                        case '7':$scope.currentSubTaskList[i].type = 'POI专项';break;
+                        case '8':$scope.currentSubTaskList[i].type = '道路_GRID精编';break;
+                        case '9':$scope.currentSubTaskList[i].type = '道路_GRID粗编';break;
+                        case '10':$scope.currentSubTaskList[i].type = '道路区域专项';break;
+                    }
+                    $scope.currentSubTaskList[i].stage = $scope.currentSubTaskList[i].stage==1?'日编':'月编';
                 }
                 /*-------------------获取所有子任务的统计数据并重新组织对象------------------*/
                 var currentIndex = 0;
@@ -95,15 +108,9 @@ angular.module('app', ['ui.layout', 'dataService', 'ngCookies','highcharts-ng','
                     })
                 }
                 requestfn();
-                /*-------------------获取所有点数组------------------*/
             });
         }
 
-        $scope.showSubList = function(param){
-            $scope.currentDeepClass = param;
-            $scope.isSubTaskListShow = !$scope.isSubTaskListShow;
-        }
-        $scope.isSubTaskListShow = false;
 
         //子任务查询
         loadSubTaskfn();
