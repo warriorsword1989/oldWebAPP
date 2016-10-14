12c7392ea7d527ca0be32cffd0233ee88be468ff
warriorsword1989 <linglong@navinfo.com>
2016-10-14 20:33:22 +0800

    执行检查

--- scripts/components/tools/ctrls/assist-tools/beginCheckPanelCtrl.js
+++ scripts/components/tools/ctrls/assist-tools/beginCheckPanelCtrl.js
@@ -5,13 +5,13 @@ angular.module('app').controller("BeginCheckPanelCtrl", ['$scope', '$interval', 
     function($scope, $interval, dsEdit, dsOutput) {
         var logMsgCtrl = fastmap.uikit.LogMsgController($scope);
         $scope.searchBoxData = [];
-        $scope.currentSearchItems = [];
 
         $scope.selectedBatches = [];
         $scope.pageSize = 10;
         $scope.page = 1;
         $scope.batchType = 0;
         $scope.dataLoading = true;
+        $scope.currentBoxIndex = 0;
 
         /**
         * 切换道路和poi批处理tab页;
@@ -25,7 +25,7 @@ angular.module('app').controller("BeginCheckPanelCtrl", ['$scope', '$interval', 
 
         //点击table行查询当前批处理包下的批处理规则;
         $scope.getCheckItem = function(param){
-            $scope.currentSearchItems = param.rules;
+            $scope.currentBoxIndex = param;
         }
 
         //获取所有批处理包;
@@ -38,80 +38,60 @@ angular.module('app').controller("BeginCheckPanelCtrl", ['$scope', '$interval', 
             }
             dsEdit.seachCheckBox(params).then(function(data){
                 $scope.searchBoxData = data;
-                //获取当前显示的检查项，默认为第一个检查项下的数据;
-                $scope.currentSearchItems = data.length?$scope.searchBoxData[0].rules:[];
                 $scope.dataLoading = false;
             });
         }
 
         //全选或反选处理;
-        //$scope.batchSelect = function(param){
-        //    if(param.checked){
-        //        for(var i=0;i<$scope.currentSearchItems.length;i++){
-        //            $scope.currentSearchItems[i].checked = true
-        //        }
-        //    }else{
-        //        for(var i=0;i<$scope.currentSearchItems.length;i++){
-        //            $scope.currentSearchItems[i].checked = false
-        //        }
-        //    }
-        //    getAllBatchRules(param);
-        //}
-        $scope.batchSelect = function(param){
-            if(param.checked){
-                for(var i=0;i<param.rules.length;i++){
-                    param.rules[i].checked = true;
+        $scope.batchSelect = function(index){
+            //$scope.currentBoxIndex = index;
+            if($scope.searchBoxData[index].checked){
+                for(var i=0;i<$scope.searchBoxData[index].rules.length;i++){
+                    $scope.searchBoxData[index].rules[i].checked = true;
                 }
             }else{
-                for(var i=0;i<param.rules.length;i++){
-                    param.rules[i].checked = false;
+                for(var i=0;i<$scope.searchBoxData[index].rules.length;i++){
+                    $scope.searchBoxData[index].rules[i].checked = false
                 }
             }
-            getAllBatchRules(param);
-        }
-
-        //单个选择处理;
-        $scope.clickBatchSelect = function(param){
-            getAllBatchRules(param);
         }
 
         //组装选中批处理规则;
-        function getAllBatchRules(param){
-            if(param.rules){//全选或反选
-                if(param.checked){
-                    for(var i=0;i<param.rules.length;i++){
-                        if($scope.selectedBatches.indexOf(param.rules[i].ruleCode)==-1){
-                            $scope.selectedBatches.push(param.rules[i].ruleCode)
-                        }
-                    }
-                }else{
-                    for(var i=0;i<param.rules.length;i++){
-                        if($scope.selectedBatches.indexOf(param.rules[i].ruleCode)!=-1){
-                            $scope.selectedBatches.splice($scope.selectedBatches.indexOf(param.rules[i].ruleCode),1);
-                        }
-                    }
-                }
-                console.log($scope.selectedBatches);
-            }else{//单选
-                if(param.checked){
-                    if($scope.selectedBatches.indexOf(param.ruleCode)==-1){
-                        $scope.selectedBatches.push(param.ruleCode)
-                    }
-                }else{
-                    if($scope.selectedBatches.indexOf(param.ruleCode)!=-1){
-                        $scope.selectedBatches.splice($scope.selectedBatches.indexOf(param.ruleCode),1);
-                    }
-                }
-                console.log($scope.selectedBatches);
-            }
-        }
-
-
-
+        //function getAllBatchRules(param){
+        //    if(param.rules){//全选或反选
+        //        if(param.checked){
+        //            for(var i=0;i<param.rules.length;i++){
+        //                if($scope.selectedBatches.indexOf(param.rules[i].ruleCode)==-1){
+        //                    $scope.selectedBatches.push(param.rules[i].ruleCode)
+        //                }
+        //            }
+        //        }else{
+        //            for(var i=0;i<param.rules.length;i++){
+        //                if($scope.selectedBatches.indexOf(param.rules[i].ruleCode)!=-1){
+        //                    $scope.selectedBatches.splice($scope.selectedBatches.indexOf(param.rules[i].ruleCode),1);
+        //                }
+        //            }
+        //        }
+        //        console.log($scope.selectedBatches);
+        //    }else{//单选
+        //        if(param.checked){
+        //            if($scope.selectedBatches.indexOf(param.ruleCode)==-1){
+        //                $scope.selectedBatches.push(param.ruleCode)
+        //            }
+        //        }else{
+        //            if($scope.selectedBatches.indexOf(param.ruleCode)!=-1){
+        //                $scope.selectedBatches.splice($scope.selectedBatches.indexOf(param.ruleCode),1);
+        //            }
+        //        }
+        //        console.log($scope.selectedBatches);
+        //    }
+        //}
 
         $scope.running = false;
         $scope.progress = 0;
         $scope.doExecute = function() {
+
+            return;
             if ($scope.selectedBatches.length == 0) {
                 swal("请选择要执行的检查项", "", "info");
                 return;
