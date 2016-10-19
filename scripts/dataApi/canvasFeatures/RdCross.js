97993d36a302b1d1def51f3a880d681550f3e0f0
wuzhen <wuzhen@navinfo.com>
2016-10-19 19:41:58 +0800

    修改路口主点高亮，增加形状点2米的控制

--- scripts/dataApi/canvasFeatures/RdCross.js
+++ scripts/dataApi/canvasFeatures/RdCross.js
@@ -4,9 +4,12 @@ fastmap.uikit.canvasFeature.RdCross = fastmap.uikit.canvasFeature.Feature.extend
         this.properties['markerStyle'] = {};
         this.properties["featType"] = "RDCROSS";
         this.properties['markerStyle']["icon"] = [];
+        this.properties['isMainArr'] = item.m.b.split(',');
+        var isMainArr = item.m.b.split(','); //1表示的是主点 0非主点
         for (var crossNum = 0, crossLen = this.geometry['coordinates'].length; crossNum < crossLen; crossNum++) {
             var crossObj = {};
-            if (crossNum === 0) {
+            //if (crossNum === 0) {
+            if (isMainArr[crossNum] == 1) { //表示的是主点
                 crossObj = fastmap.uikit.canvasFeature.Feature.getIconStyle({
                         iconName: '../../../images/road/rdcross/11.png',
                         row: 0,
