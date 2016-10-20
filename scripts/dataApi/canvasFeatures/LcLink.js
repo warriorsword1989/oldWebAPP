8291dfbe5c2baa40e65756ebc2216019c73e1332
warriorsword1989 <linglong@navinfo.com>
2016-10-20 19:44:06 +0800

    解决bug#405；bug#794；bug#736

--- scripts/dataApi/canvasFeatures/LcLink.js
+++ scripts/dataApi/canvasFeatures/LcLink.js
@@ -8,8 +8,8 @@ fastmap.uikit.canvasFeature.LCLink = fastmap.uikit.canvasFeature.Feature.extend(
         this.properties['snode'] = item.m.a;
         this.properties['enode'] = item.m.b;
         this.properties['kind'] = item.m.c;
-        this.properties['style']['strokeColor'] = '#FF9900';
-        this.properties['style']['strokeWidth'] = 3;
+        this.properties['style']['strokeColor'] = (item.m.c == 21)?'#8fefd5':(item.m.c == 22)?'#50b450':'#8fefd5';
+        this.properties['style']['strokeWidth'] = 2;
         this.properties['style']['strokeOpacity'] = 1;
     }
 });
