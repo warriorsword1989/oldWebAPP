1687df15888891a74e656ae78b0b0700a0d42a62
warriorsword1989 <linglong@navinfo.com>
2016-10-24 11:04:40 +0800

    高亮点颜色改回yellow

--- scripts/uikits/controllers/HighRenderController.js
+++ scripts/uikits/controllers/HighRenderController.js
@@ -112,7 +112,7 @@ fastmap.uikit.HighRenderController = (function () {
                       this.layer._drawPoint({
                         boolPixelCrs: true,
                         ctx: ctx,
-                        fillColor: 'red',
+                        fillColor: 'yellow',
                         radius: 4,
                         geom: geo
                       })
