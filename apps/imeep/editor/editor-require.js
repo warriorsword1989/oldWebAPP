2ae8877644fcf0a0a0677f7eef11017704270b38
warriorsword1989 <linglong@navinfo.com>
2016-10-25 11:24:16 +0800

    执行检查根据当前任务显示

--- apps/imeep/editor/editor-require.js
+++ apps/imeep/editor/editor-require.js
@@ -41,7 +41,7 @@ requirejs.config({
         'fmEditView': 'scripts/components/directives/fmEditView/fmEditView',
         'langcodeFilter': 'scripts/components/filter/langCodeFilter'
     },
-//    urlArgs: "bust=" + (new Date()).getTime(),
+    urlArgs: "bust=" + (new Date()).getTime(),
     waitSeconds: 0,
     shim: {
         'angular': {
--- apps/imeep/editor/editorCtl.js
+++ apps/imeep/editor/editorCtl.js
@@ -435,6 +435,7 @@ angular.module('app', ['ngCookies', 'oc.lazyLoad', 'fastmap.uikit', 'ui.layout',
 					App.Temp.dbId = data.dbId;
                     App.Temp.taskType = data.type;
 					App.Temp.gridList = data.gridIds;
+                    $scope.currentSubTaskType = data.type;
 					if (data.stage == 1) { // 日编
 						App.Temp.mdFlag = "d";
 					} else if (data.stage == 2) { // 月编
