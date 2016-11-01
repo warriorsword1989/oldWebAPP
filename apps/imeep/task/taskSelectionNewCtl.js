5944ef9bf9d9defda87a8c436bd807356ae52053
wuzhen <wuzhen@navinfo.com>
2016-11-01 15:00:17 +0800

    增加子任务提交功能

--- apps/imeep/task/taskSelectionNewCtl.js
+++ apps/imeep/task/taskSelectionNewCtl.js
@@ -42,15 +42,15 @@ angular.module('app', ['ui.layout', 'dataService', 'ngCookies', 'ui.bootstrap'])
             $scope.sortCondtion = $scope.sortOrient + $scope.sortCondtion.substr(1);
         }
         $scope.goEditorPage = function(param) {
-                $cookies.remove('IMEEP_EDITOR_MAP_ZOOM', {
-                    path: '/'
-                });
-                $cookies.remove('IMEEP_EDITOR_MAP_CENTER', {
-                    path: '/'
-                });
-                window.location.href = "../editor/editor.html?access_token=" + App.Temp.accessToken + "&subtaskId=" + param;
-            }
-            /*切换当前作业和历史作业tab页*/
+            $cookies.remove('IMEEP_EDITOR_MAP_ZOOM', {
+                path: '/'
+            });
+            $cookies.remove('IMEEP_EDITOR_MAP_CENTER', {
+                path: '/'
+            });
+            window.location.href = "../editor/editor.html?access_token=" + App.Temp.accessToken + "&subtaskId=" + param;
+        }
+        /*切换当前作业和历史作业tab页*/
         $scope.chnageTab = function(param) {
             $scope.currentTab = param;
             if ($scope.currentTab == 1) {
@@ -63,7 +63,16 @@ angular.module('app', ['ui.layout', 'dataService', 'ngCookies', 'ui.bootstrap'])
                 $scope.showTask = false;
             }
             loadSubTaskfn();
-        }
+        };
+
+        $scope.submitTask = function (subTaskId){
+            dsManage.submitTask(subTaskId).then(function (data){
+                if(data){
+                    loadSubTaskfn();
+                }
+            });
+        };
+
         $scope.logout = function() {
                 App.Util.logout();
             }
