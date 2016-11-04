553ccbe29f8ba7ea8c2ed66434d7ad9fe83d6b64
wangmingdong <wangmingdong@navinfo.com>
2016-11-04 15:45:46 +0800

    修改高速分歧tips、道路方向tips渲染角度

--- scripts/dataApi/canvasFeatures/TipsOrientation.js
+++ scripts/dataApi/canvasFeatures/TipsOrientation.js
@@ -6,7 +6,7 @@ fastmap.uikit.canvasTips.TipsOrientation = fastmap.uikit.canvasTips.Tips.extend(
                 iconName: '../../../images/road/tips/1401/1401_0_0.svg',
                 row: 0,
                 column: 1,
-                rotate: (item.m.c-180) * (Math.PI / 180),
+                rotate: (item.m.c) * (Math.PI / 180),
                 location: this.geometry['coordinates'],
                 fillStyle:item.m.a == "0"?this.redFill:this.blueFill
             })
