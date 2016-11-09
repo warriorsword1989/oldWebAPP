938cd09a8bb598d9a4d0a28a23828f6e4eab88f1
wuzhen <wuzhen@navinfo.com>
2016-11-09 15:34:19 +0800

    增加tips类型

--- scripts/components/tools/ctrls/toolbar-map/selectShapeCtrl.js
+++ scripts/components/tools/ctrls/toolbar-map/selectShapeCtrl.js
@@ -4578,7 +4578,7 @@ angular.module("app").controller("selectShapeCtrl", ["$scope", '$q', '$ocLazyLoa
             highRenderCtrl.drawHighlight();
             //wt改，注释前会因为地图移动导致弹出工具条位置有偏移
             //map.setView([objCtrl.data.geometry.coordinates[1], objCtrl.data.geometry.coordinates[0]]);
-            console.log('-----------------'+[objCtrl.data.geometry.coordinates[1], objCtrl.data.geometry.coordinates[0]])
+            //console.log('-----------------'+[objCtrl.data.geometry.coordinates[1], objCtrl.data.geometry.coordinates[0]])
         }
     }
 ]);
