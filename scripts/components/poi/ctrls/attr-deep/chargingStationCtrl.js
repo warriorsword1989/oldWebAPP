f43f4ccf968f3ac785e9e2ec9a6f72dc580cf676
wuzhen <wuzhen@navinfo.com>
2016-11-15 15:29:19 +0800

    充电站联调

--- scripts/components/poi/ctrls/attr-deep/chargingStationCtrl.js
+++ scripts/components/poi/ctrls/attr-deep/chargingStationCtrl.js
@@ -7,25 +7,25 @@ angular.module('app').controller('chargingStationCtrl', function($scope) {
     {"id": 4, "label": "换电站"}
     ];
     $scope.serviceProvArr = [
-        {"id": 0, "label": "其他"},
-        {"id": 1, "label": "国家电网"},
-        {"id": 2, "label": "南方电网"},
-        {"id": 3, "label": "中石油"},
-        {"id": 4, "label": "中石化"},
-        {"id": 5, "label": "中海油"},
-        {"id": 6, "label": "中国普天"},
-        {"id": 7, "label": "特来电"},
-        {"id": 8, "label": "循道新能源"},
-        {"id": 9, "label": "富电科技"},
-        {"id": 10, "label": "华商三优"},
-        {"id": 11, "label": "中電"},
-        {"id": 12, "label": "港燈"},
-        {"id": 13, "label": "澳電"},
-        {"id": 14, "label": "绿狗"},
-        {"id": 15, "label": "EVCARD"},
-        {"id": 16, "label": "星星充电"},
-        {"id": 17, "label": "电桩"},
-        {"id": 18, "label": "依威能源"}
+        {"id": '0', "label": "其他"},
+        {"id": '1', "label": "国家电网"},
+        {"id": '2', "label": "南方电网"},
+        {"id": '3', "label": "中石油"},
+        {"id": '4', "label": "中石化"},
+        {"id": '5', "label": "中海油"},
+        {"id": '6', "label": "中国普天"},
+        {"id": '7', "label": "特来电"},
+        {"id": '8', "label": "循道新能源"},
+        {"id": '9', "label": "富电科技"},
+        {"id": '10', "label": "华商三优"},
+        {"id": '11', "label": "中電"},
+        {"id": '12', "label": "港燈"},
+        {"id": '13', "label": "澳電"},
+        {"id": '14', "label": "绿狗"},
+        {"id": '15', "label": "EVCARD"},
+        {"id": '16', "label": "星星充电"},
+        {"id": '17', "label": "电桩"},
+        {"id": '18', "label": "依威能源"}
     ];
     $scope.chargingOpenType = FM.dataApi.Constant.chargingOpenType;
     $scope.chargingOpenTypeChange = function(event){
