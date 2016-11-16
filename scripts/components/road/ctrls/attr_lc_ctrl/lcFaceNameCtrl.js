fe7eb4c522087a15d5d99fb4a7b509bb6e5d4b34
wangmingdong <wangmingdong@navinfo.com>
2016-11-16 20:47:08 +0800

    LCFace名称信息修改

--- scripts/components/road/ctrls/attr_lc_ctrl/lcFaceNameCtrl.js
+++ scripts/components/road/ctrls/attr_lc_ctrl/lcFaceNameCtrl.js
@@ -2,7 +2,7 @@
  * Created by mali on 2016/10/21.
  */
 
-angular.module('app').controller('LcFaceNameCtl', ['$scope', 'dsEdit', 'dsMeta', function ($scope, dsEdit, dsMeta) {
+angular.module('app').controller('LcFaceNameCtl', ['$scope', 'dsEdit', 'dsMeta', '$timeout', function ($scope, dsEdit, dsMeta, $timeout) {
     var objCtrl = fastmap.uikit.ObjectEditController();
     $scope.lcFaceNames = objCtrl.namesInfos;
     $scope.selectedLangcodeArr = [];
@@ -31,28 +31,55 @@ angular.module('app').controller('LcFaceNameCtl', ['$scope', 'dsEdit', 'dsMeta',
     };
 	// 增加名称信息
     $scope.addNameInfo = function () {
-        for (var i = 0; i < $scope.langCodeOptions.length; i++) {
-            for (var j = 0; j < $scope.lcFaceNames.length; j++) {
-                var flag = false;
-                if ($scope.langCodeOptions[i].id == $scope.lcFaceNames[j].langCode) {
-                    break;
-                }
-                if ($scope.langCodeOptions[i].id != $scope.lcFaceNames[j].langCode && j == $scope.lcFaceNames.length - 1) {
-                    $scope.lcFaceNames.push(fastmap.dataApi.lcFaceName({ nameGroupid: $scope.lcFaceNames[0].nameGroupid, langCode: $scope.langCodeOptions[i].id }));
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag) {
-                break;
-            }
-        }
-//		$scope.tollGateNames.push(fastmap.dataApi.rdTollgateName({nameGroupid:$scope.tollGateNames[0].nameGroupid}));
+	    getSelectedLangcode();
+	    for (var i = 0; i < $scope.langCodeOptions.length; i++) {
+		    if ($scope.selectedLangcodeArr.indexOf($scope.langCodeOptions[i].id) === -1) {
+			    if (($scope.selectedLangcodeArr.indexOf('CHI') > -1 || $scope.selectedLangcodeArr.indexOf('CHT') > -1) && ($scope.langCodeOptions[i].id === 'CHI' || $scope.langCodeOptions[i].id === 'CHT')) {
+			    } else {
+				    $scope.lcFaceNames.push(fastmap.dataApi.lcFaceName({ nameGroupid: $scope.lcFaceNames[0].nameGroupid, langCode: $scope.langCodeOptions[i].id }));
+				    break;
+			    }
+		    }
+	    }
+	    $scope.refreshNameLangCode();
     };
 	// 代码语言字段切换时，判断语言不能重复
-    $scope.langCodeChange = function (event, obj) {
-        getSelectedLangcode();
-    };
+	$scope.langCodeChange = function (langCode) {
+		// 如果当前所选既不是简体也不是繁体，则控制不允许选择简繁体
+		getSelectedLangcode();
+		if (langCode != 'CHI' && langCode != 'CHT') {
+			if ($scope.selectedLangcodeArr.indexOf('CHI') === -1) {
+				$scope.selectedLangcodeArr.push('CHI');
+			}
+			if ($scope.selectedLangcodeArr.indexOf('CHT') === -1) {
+				$scope.selectedLangcodeArr.push('CHT');
+			}
+		} else if (langCode == 'CHI') { // 如果是简体中文或繁体中文其他语言不可用
+			$scope.selectedLangcodeArr = [];
+			for (var i = 0; i < $scope.langCodeOptions.length; i++) {
+				if ($scope.langCodeOptions[i].id != 'CHT') {
+					$scope.selectedLangcodeArr.push($scope.langCodeOptions[i].id);
+				}
+			}
+		} else if (langCode == 'CHT') {
+			$scope.selectedLangcodeArr = [];
+			for (var i = 0; i < $scope.langCodeOptions.length; i++) {
+				if ($scope.langCodeOptions[i].id != 'CHI') {
+					$scope.selectedLangcodeArr.push($scope.langCodeOptions[i].id);
+				}
+			}
+		}
+		$scope.refreshNameLangCode();
+		$timeout(function () {
+			$scope.$apply();
+		});
+	};
+	// 重新排列名称信息
+	$scope.refreshNameLangCode = function () {
+		$scope.lcFaceNames.sort(function (a, b) {
+			return $scope.langCodeRelation[a.langCode] - $scope.langCodeRelation[b.langCode];
+		});
+	};
 	/* 名称来源*/
     $scope.nameSource = [
         { code: 0, label: '未定义' },
@@ -92,4 +119,39 @@ angular.module('app').controller('LcFaceNameCtl', ['$scope', 'dsEdit', 'dsMeta',
 		{ id: 'UKR', label: '乌克兰语' },
 		{ id: 'SCR', label: '克罗地亚语' }
     ];
+	// 语言代码对应关系
+	$scope.langCodeRelation = {
+		CHI: 1,
+		CHT: 2,
+		ENG: 3,
+		POR: 4,
+		ARA: 5,
+		BUL: 6,
+		CZE: 7,
+		DAN: 8,
+		DUT: 9,
+		EST: 10,
+		FIN: 11,
+		FRE: 12,
+		GER: 13,
+		HIN: 14,
+		HUN: 15,
+		ICE: 16,
+		IND: 17,
+		ITA: 18,
+		JPN: 19,
+		KOR: 20,
+		LIT: 21,
+		NOR: 22,
+		POL: 23,
+		RUM: 24,
+		RUS: 25,
+		SLO: 26,
+		SPA: 27,
+		SWE: 28,
+		THA: 29,
+		TUR: 30,
+		UKR: 31,
+		SCR: 32
+	};
 }]);
