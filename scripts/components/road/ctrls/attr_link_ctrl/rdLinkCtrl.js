6355b982a32477a550dd9f7aff07173b7ef06139
wangtun <wangtun@navinfo.com>
2016-11-26 15:36:54 +0800

    修改语法错误

--- scripts/components/road/ctrls/attr_link_ctrl/rdLinkCtrl.js
+++ scripts/components/road/ctrls/attr_link_ctrl/rdLinkCtrl.js
@@ -530,16 +530,16 @@ angular.module('app').controller('linkObjectController', ['$scope', '$ocLazyLoad
         }
         /* 如果道路名新增*/
         if ($scope.linkData.names) {
-        	var flag = false;
+            var flag = false;
             $.each($scope.linkData.names, function (i, v) {
-            	if (v.nameGroupid == 0) {
-            		flag = true;
-            	}
+                if (v.nameGroupid == 0) {
+                    flag = true;
+                }
                 if (v.pid) delete v.pid;
             });
             if (flag) {
-            	swal('保存提示', '道路名不合法(合法的道路名应该来源于道路名库)', 'error');
-            	return;
+                swal('保存提示', '道路名不合法(合法的道路名应该来源于道路名库)', 'error');
+                return;
             }
         }
 
